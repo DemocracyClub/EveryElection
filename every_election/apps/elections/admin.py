3e135c46fb651190fceb01905c21c4190fe9c830
Sym Roe <sym.roe@talusdesign.co.uk>
2017-01-09 16:29:44 +0000

    Add Election Admin interface

--- every_election/apps/elections/admin.py
+++ every_election/apps/elections/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Election
+
+class ElectionAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
+
+    readonly_fields = (
+        'election_id',
+        'tmp_election_id',
+        'election_type',
+        'election_subtype',
+        'poll_open_date',
+        'organisation',
+        'elected_role',
+        'division',
+        'seats_contested',
+        'seats_total',
+        'group',
+    )
+
+admin.site.register(Election, ElectionAdmin)
--- every_election/urls.py
+++ every_election/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib import admin
 
 from django.views.generic import TemplateView
 
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='home.html'), name="home"),
     url(r'^organisations/', include('organisations.urls')),
     url(r'', include('elections.urls')),
