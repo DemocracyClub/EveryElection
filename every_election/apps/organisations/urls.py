1dc1d37bd387c2d0421e8c583db48cfe805caa73
Sym Roe <sym.roe@talusdesign.co.uk>
2017-01-16 18:03:48 +0000

    Add Org detail view

--- every_election/apps/organisations/urls.py
+++ every_election/apps/organisations/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url
 
-from .views import SupportedOrganisationsView
+from .views import SupportedOrganisationsView, OrganisationsView
 
 urlpatterns = [
     url(
         r'^$',
         SupportedOrganisationsView.as_view(),
         name='organisations_view'),
+    url(
+        r'^(?P<official_identifier>.+)/$',
+        OrganisationsView.as_view(),
+        name='organisation_view'),
 ]
--- every_election/apps/organisations/views.py
+++ every_election/apps/organisations/views.py
@@ -1,7 +1,12 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Organisation
 
 class SupportedOrganisationsView(ListView):
     template_name = "organisations/supported_organisations.html"
     queryset = Organisation.objects.all().order_by('organisation_type', 'common_name')
+
+class OrganisationsView(DetailView):
+    queryset = Organisation.objects.all()
+    slug_url_kwarg = 'official_identifier'
+    slug_field = 'official_identifier'
