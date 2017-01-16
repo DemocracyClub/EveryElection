9d3e09ba014edeff48ff5cf4c14d1bad9a8f0244
Sym Roe <sym.roe@talusdesign.co.uk>
2017-01-16 18:02:40 +0000

    Add OrganisationDivisionSet

--- every_election/apps/organisations/management/commands/import_organisation_divisions.py
+++ every_election/apps/organisations/management/commands/import_organisation_divisions.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 import time
 
 import requests
@@ -9,7 +10,11 @@ CACHE = requests_cache.get_cache()
 from django.core.management.base import BaseCommand
 from django.utils.text import slugify
 
-from organisations.models import Organisation, OrganisationDivision
+from organisations.models import (
+    Organisation,
+    OrganisationDivision,
+    OrganisationDivisionSet
+)
 from organisations.constants import PARENT_TO_CHILD_AREAS
 
 
@@ -44,31 +49,170 @@ class Command(BaseCommand):
 
             self.import_divisions(organisation, req.json())
 
+    def load_mapit_generations(self):
+        url = "{}/generations".format(self.BASE)
+        self.mapit_generations = requests.get(url).json()
+        for generation_id, generation in self.mapit_generations.items():
+            generation['uri'] = "{}/{}".format(url, generation_id)
+
+
+    def _create_division_set(self, organisation, mapit_generation_uri,
+            mapit_generation_start_date):
+        division_set, _ = OrganisationDivisionSet.objects.update_or_create(
+            organisation=organisation,
+            mapit_generation_id=mapit_generation_uri,
+            defaults={
+                'start_date': mapit_generation_start_date,
+                'short_title': "{} Boundaries".format(
+                    mapit_generation_start_date[:4]),
+                'notes': "Auto imported from {}".format(self.BASE),
+            }
+        )
+        return division_set
+
+    def get_division_set(self, organisation, division):
+        mapit_generation = self.mapit_generations.get(
+            str(division['generation_low'])
+        )
+        mapit_generation_uri = mapit_generation['uri']
+        mapit_generation_start_date = mapit_generation['created'].split('T')[0]
+
+
+        try:
+            division_set = OrganisationDivisionSet.objects.get(
+                mapit_generation_id=mapit_generation_uri,
+                organisation=organisation
+            )
+        except OrganisationDivisionSet.DoesNotExist:
+            # We've not recoreded that we know about this OrganisationDivisionSet yet so we…
+            # 1 Figure out if it's because it's a new one from MapIt or not
+            existing_division_sets = OrganisationDivisionSet.objects.filter(
+                organisation=organisation,
+                start_date__lte=mapit_generation_start_date
+            )
+            if not existing_division_sets:
+                # There are no sets before this mapit set, so we assume it's
+                # just brand new to us (first import, or we've not manually
+                # created one)
+                division_set = self._create_division_set(
+                    organisation,
+                    mapit_generation_uri, mapit_generation_start_date
+                    )
+            else:
+                ds = existing_division_sets.first()
+                print("This might be a new OrganisationDivisionSet…")
+                print('Found a MaPit generation "{}" ("{}") dated {}'.format(
+                    mapit_generation_uri,
+                    mapit_generation['description'],
+                    mapit_generation_start_date,
+                ))
+                msg = " ".join([
+                "We know about an existing division set starting on {} ",
+                """with the title "{}" and note "{}". """,
+                """\nThere are 2 options:""",
+                """\n 1. Assert that the existing set is part of this""",
+                """MaPit generation""",
+                """\n 2. Make a new generation, and set the end date of""",
+                """the new generation to the day before the start date""",
+                """of the new one""",
+                """\n\nIf you're not sure you should quit this script and""",
+                """figure it out another way""",
+                ])
+                msg = msg.format(
+                        ds.start_date,
+                        ds.short_title,
+                        ds.notes,
+                    )
+                print(msg)
+                if self.always_pick_option in [1,2]:
+                    decision = self.always_pick_option
+                else:
+                    decision = int(input("Enter either 1 or 2: "))
+                if decision == 1:
+                    ds.mapit_generation_uri = mapit_generation_uri
+                    ds.save()
+                    division_set = ds
+                if decision == 2:
+                    start_date = datetime.strptime(
+                        mapit_generation['created'].split('T')[0], "%Y-%m-%d")
+                    ds.end_date = start_date - timedelta(days=1)
+                    ds.save()
+                    division_set = self._create_division_set(
+                        organisation,
+                        mapit_generation_uri, mapit_generation_start_date
+                        )
+        return division_set
+
+    def carry_over_existing_divisions(self, organisation):
+        print("Carrying over…")
+        all_sets = organisation.divisionset.all().order_by('start_date')
+        if all_sets.count() == 1:
+            print("Only 1 set, carrying on")
+            return
+
+        for div_set in all_sets:
+            try:
+                newer_set = organisation.divisionset.filter(
+                    start_date__gt=div_set.start_date
+                    ).order_by('start_date').first()
+            except OrganisationDivisionSet.DoesNotExist:
+                print("No more sets")
+                newer_set = None
+            if not newer_set:
+                return
+
+            new_gen_id = int(newer_set.mapit_generation_id.split('/')[-1])
+            carry_over = div_set.divisions.filter(
+                mapit_generation_high__gte=new_gen_id)
+
+            start_date = newer_set.start_date
+            div_set.end_date = start_date - timedelta(days=1)
+            div_set.save()
+
+
+            for div in carry_over:
+                try:
+                    # Try to get the existing div for this set
+                    newer_set.divisions.get(name=div.name)
+                except OrganisationDivision.DoesNotExist:
+                    # Make a new set
+                    div.pk = None
+                    div.divisionset = newer_set
+                    div.save()
+
     def import_divisions(self, organisation, data):
         for mapit_id, division in data.items():
 
-            try:
-                ons = division['codes'].get('ons')
-                unit_id = division['codes'].get('unit_id')
-                gss = division['codes'].get('gss')
-                all_codes = [x for x in [ons, unit_id, gss] if x]
+            division_set = self.get_division_set(organisation, division)
 
-                official_identifier = all_codes[0]
+            try:
+                all_codes = [
+                    ('gss', division['codes'].get('gss')),
+                    ('ons', division['codes'].get('ons')),
+                    ('unit_id', division['codes'].get('unit_id')),
+                ]
+                all_codes = [x for x in all_codes if x[1]]
+
+                official_identifier = ":".join(all_codes[0])
             except:
-                print(all_codes)
                 print(division)
 
             OrganisationDivision.objects.update_or_create(
                 official_identifier=official_identifier,
                 organisation=organisation,
+                divisionset=division_set,
                 defaults={
-                    'gss': division['codes'].get('gss', ''),
+                    'geography_curie': official_identifier,
                     'name': division['name'],
                     'slug': slugify(division['name']),
                     'division_type': division['type'],
                     'division_subtype': division['type_name'],
+                    'mapit_generation_low': int(division['generation_low']),
+                    'mapit_generation_high': int(division['generation_high']),
+
                 }
             )
+        self.carry_over_existing_divisions(organisation)
 
     def import_ni_areas(self):
         """
--- every_election/apps/organisations/models.py
+++ every_election/apps/organisations/models.py
@@ -28,6 +28,24 @@ class Organisation(models.Model):
     class Meta:
         ordering = ('official_name',)
 
+    def get_absolute_url(self):
+        return reverse("organisation_view", args=(self.official_identifier,))
+
+
+class OrganisationDivisionSet(models.Model):
+    organisation = models.ForeignKey(Organisation, related_name='divisionset')
+    start_date = models.DateField(null=True)
+    end_date = models.DateField(null=True)
+    legislation_url = models.CharField(blank=True, max_length=500, null=True)
+    short_title = models.CharField(blank=True, max_length=200)
+    mapit_generation_id = models.CharField(blank=True, max_length=255)
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return "{}".format(self.short_title)
+
+    class Meta:
+        ordering = ('-start_date',)
 
 class OrganisationDivision(models.Model):
     """
@@ -35,12 +53,30 @@ class OrganisationDivision(models.Model):
 
     This could be a ward, constituency or office
     """
-    organisation = models.ForeignKey(Organisation)
+    organisation = models.ForeignKey(Organisation, related_name='divisions')
+    divisionset = models.ForeignKey(
+        OrganisationDivisionSet, related_name='divisions', null=True)
     name = models.CharField(blank=True, max_length=255)
     official_identifier = models.CharField(
         blank=True, max_length=255, db_index=True)
-    gss = models.CharField(blank=True, max_length=20)
+    geography_curie = models.CharField(blank=True, max_length=100)
     slug = models.CharField(blank=True, max_length=100)
     division_type = models.CharField(blank=True, max_length=255)
     division_subtype = models.CharField(blank=True, max_length=255)
     division_election_sub_type = models.CharField(blank=True, max_length=2)
+    seats_total = models.IntegerField(blank=True, null=True)
+    mapit_generation_low = models.IntegerField(blank=True, null=True)
+    mapit_generation_high = models.IntegerField(blank=True, null=True)
+
+
+    def __str__(self):
+        return "{}".format(self.name)
+
+    class Meta:
+        ordering = ('name',)
+
+    def format_geography_link(self):
+        code_type, code = self.geography_curie.split(':')
+        return "https://mapit.mysociety.org/code/{}/{}".format(
+            code_type, code
+        )
