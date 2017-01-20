a49b23eb0e5f2cb81a2c5b0074230d6ec9a47152
Sym Roe <sym.roe@talusdesign.co.uk>
2017-01-20 14:55:49 +0000

    New home page

--- every_election/apps/core/views.py
+++ every_election/apps/core/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from elections.models import Election
+
+class HomeView(TemplateView):
+    template_name='home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+
+        election_qs = Election.objects.all()
+        election_qs = election_qs.filter(group_type='election')
+        election_qs = election_qs.order_by('-poll_open_date')[:5]
+        context['upcoming_elections'] = election_qs
+
+        return context
--- every_election/urls.py
+++ every_election/urls.py
@@ -3,12 +3,11 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 
-from django.views.generic import TemplateView
-
+from core.views import HomeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='home.html'), name="home"),
+    url(r'^$', HomeView.as_view(), name="home"),
     url(r'^organisations/', include('organisations.urls')),
     url(r'', include('elections.urls')),
     url(r'^api/', include('api.urls')),
