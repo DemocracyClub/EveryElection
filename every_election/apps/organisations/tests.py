1bdaed4c1859690ff8e77501d086af93bec49e5c
Sym Roe <sym.roe@talusdesign.co.uk>
2017-03-10 10:10:14 +0000

    pytest and flake8

--- every_election/apps/organisations/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-# from organisations.models import Organisation
-
-
-class TestElectionIDs(TestCase):
-    def test_example_election_id(self):
-        pass
--- every_election/settings/base.py
+++ every_election/settings/base.py
@@ -164,6 +164,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
 }
 
+
 # .local.py overrides all the common settings.
 try:
     from .local import *  # noqa
