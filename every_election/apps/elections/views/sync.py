b50f298e0206cb8882dc7347e5b220b8d3fb4dc6
chris48s <chris.shaw480@gmail.com>
2017-08-08 12:14:08 +0100

    run dumpdata on schedule instead of on the fly

--- every_election/apps/elections/views/sync.py
+++ every_election/apps/elections/views/sync.py
@@ -1,12 +1,9 @@
-from io import StringIO
-from django.core.management import call_command
+import os
+from django.conf import settings
 from django.http import HttpResponse
 
 
 def get_election_fixture(request):
-
-    out = StringIO()
-    call_command('dumpdata', 'elections', stdout=out)
-
-    return HttpResponse(out.getvalue(),
+    out = open(os.path.join(settings.BASE_DIR, "data/elections.json")).read()
+    return HttpResponse(out,
         status=200, content_type='application/json')
