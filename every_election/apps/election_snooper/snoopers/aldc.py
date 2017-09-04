d1a297816fef31355a7baab53986d6ab90af8dfd
chris48s <chris.shaw480@gmail.com>
2017-09-04 10:12:06 +0100

    also tolerate unparseable date in ALDC scraper

--- every_election/apps/election_snooper/snoopers/aldc.py
+++ every_election/apps/election_snooper/snoopers/aldc.py
@@ -21,7 +21,6 @@ class ALDCScraper(BaseSnooper):
                 'div', {'class': 'election-heading'}).a['href'].strip()
             content = tile.find(
                 'div', {'class': 'election-content'}).find_all('p')
-            date = datetime.strptime(content[0].strong.text, "%B %d, %Y")
 
             if 'cause' in content[1].text.lower():
                 seat_control, cause = content[1].text.lower().split('cause')
@@ -31,11 +30,15 @@ class ALDCScraper(BaseSnooper):
             data = {
                 'title': title,
                 'source': url,
-                'date': date,
                 'cause': cause,
                 'detail': "\n".join([x.text for x in content]),
                 'snooper_name': self.snooper_name,
             }
+            try:
+                data['date'] = datetime.strptime(content[0].strong.text, "%B %d, %Y")
+            except ValueError:
+                pass
+
             item, created = SnoopedElection.objects.update_or_create(
                 snooper_name=self.snooper_name,
                 detail_url=detail_url,
