c55b715d61238573585aabde76b074868e92884f
chris48s <chris.shaw480@gmail.com>
2018-01-22 16:05:05 +0000

    Improve script for importing DivisionSets
    This commit does several things:
    
    1. Instead of setting the end date on the previous DivisionSet
       based on the start date of the new one, allow the start date
       of the new DivisionSet to be inferred from the end date of
       the old one (+ perform necessary validation)
    2. Build all our objects and then persist them in a transaction.
       This reduces the chance of a bad data point leaving our
       database in an inconsistent state.
    3. Assume CSV encoding is utf-8 if importing from google docs.
       Google docs doesn't serve up an encoding header so requests
       assumes ISO-8859-1 (because that's what the spec says).
       This was giving me some encoding errors. Manually
       setting to utf-8 fixes.

--- every_election/apps/organisations/management/commands/import_divisionsets_from_csv.py
+++ every_election/apps/organisations/management/commands/import_divisionsets_from_csv.py
@@ -1,8 +1,10 @@
 import csv
-
+import re
 import requests
+from datetime import timedelta
 
 from django.core.management.base import BaseCommand
+from django.db import transaction
 from django.utils.text import slugify
 
 from organisations.models import (
@@ -10,7 +12,6 @@ from organisations.models import (
     OrganisationDivision,
     OrganisationDivisionSet
 )
-from organisations.utils import add_end_date_to_previous_div_sets
 from organisations.constants import (
     ORG_CURIE_TO_MAPIT_AREA_TYPE, PARENT_TO_CHILD_AREAS)
 
@@ -21,36 +22,82 @@ class Command(BaseCommand):
         seats_total, Boundary Commission Consultation URL, Legislation URL,
         Short Title, Notes, Mapit Generation URI, Organisation ID"""
 
+    # dict of DivisionSet objects keyed by organisation
+    # (because we can't have >1 active DivisionSets for an organisation)
+    division_sets = {}
+    # list of divisions
+    divisions = []
+
     def add_arguments(self, parser):
         parser.add_argument('url', action='store')
 
     def handle(self, *args, **options):
         self.org_curie_to_area_type = ORG_CURIE_TO_MAPIT_AREA_TYPE
-        url = options['url']
-        csv_reader = csv.DictReader(requests.get(url).text.splitlines())
-        for line in csv_reader:
-            self.add_division(line)
+
+        csv_data = self.get_data(options['url'])
+
+        # first pass over the csv builds the division sets
+        self.create_division_sets(csv_data)
+
+        # second pass over the csv builds the divisions
+        self.create_divisions(csv_data)
+
+        # now we've created all the objects,
+        # save them all inside a transaction
+        self.save_all()
+
+    def get_data(self, url):
+        r = requests.get(url)
+
+        # if CSV came from google docs
+        # manually set the encoding
+        gdocs_pattern = r'(.)+docs\.google(.)+\/ccc(.)+'
+        if re.match(gdocs_pattern, url):
+            r.encoding = 'utf-8'
+
+        csv_reader = csv.DictReader(r.text.splitlines())
+        return list(csv_reader)
 
     def get_org_from_line(self, line):
         return Organisation.objects.get(
             official_identifier=line['Organisation ID'])
 
-    def get_div_set(self, org, line):
-        div_set, _ = OrganisationDivisionSet.objects.update_or_create(
-            organisation=org,
-            start_date=line['Start Date'],
-            defaults={
-                'end_date': line['End Date'] or None,
-                'legislation_url': line['Legislation URL'],
-                'short_title': line['Short Title'],
-                'mapit_generation_id': line['Mapit Generation URI'],
-                'notes': line['Notes'],
-                'consultation_url':
-                    line['Boundary Commission Consultation URL'],
+    def get_start_date(self, org):
+        # Given an org, work out the start date for a new division set
+        # based on the end date of the most recent previous division set
+        divsets = OrganisationDivisionSet.objects\
+            .filter(organisation=org)\
+            .order_by('-start_date')
+        if not divsets:
+            raise Exception('Could not find any previous DivisionSets for Organisation %s' % org)
+        if not divsets[0].end_date:
+            raise Exception('End date for previous DivisionSets %s is NULL' % divsets[0])
+        return divsets[0].end_date + timedelta(days=1)
 
-            })
-        add_end_date_to_previous_div_sets(div_set)
-        return div_set
+    def create_division_sets(self, csv_data):
+        for line in csv_data:
+            org = self.get_org_from_line(line)
+
+            if line['Start Date']:
+                # if we have specified a start date, use that
+                # we might need to do this if we are importing
+                # division set data for a new organisation
+                start_date = line['Start Date']
+            else:
+                # otherwise, infer the start date based on
+                # the end date of the previous DivisionSet
+                start_date = self.get_start_date(org)
+
+            self.division_sets[org.official_identifier] = OrganisationDivisionSet(
+                organisation=org,
+                start_date=start_date,
+                end_date=line['End Date'] or None,
+                legislation_url=line['Legislation URL'],
+                short_title=line['Short Title'],
+                mapit_generation_id=line['Mapit Generation URI'],
+                notes=line['Notes'],
+                consultation_url=line['Boundary Commission Consultation URL']
+            )
 
     def name_to_id(self, name):
         name = name.replace("&", "and")
@@ -89,7 +136,7 @@ class Command(BaseCommand):
         ])
         return PARENT_TO_CHILD_AREAS[self.org_curie_to_area_type[curie]][0]
 
-    def create_div_from_line(self, div_set, identifier, line):
+    def create_div_from_line(self, org, identifier, line):
         if line['geography_curie']:
             geography_curie = line['geography_curie']
         else:
@@ -99,24 +146,34 @@ class Command(BaseCommand):
         if not seats_total:
             seats_total = 1
 
-        div, _ = OrganisationDivision.objects.update_or_create(
+        div = OrganisationDivision(
             official_identifier=identifier,
-            organisation=div_set.organisation,
-            divisionset=div_set,
-            defaults={
-                'geography_curie': geography_curie,
-                'name': line['Name'],
-                'slug': slugify(line['Name']),
-                'division_type':
-                    self.get_division_type_from_registers(line),
-                'seats_total': seats_total,
-            }
+            organisation=org,
+            geography_curie=geography_curie,
+            name=line['Name'],
+            slug=slugify(line['Name']),
+            division_type=self.get_division_type_from_registers(line),
+            seats_total=seats_total,
         )
+        # set a NULL placeholder for now. We'll update
+        # it once the DivisionSets have been persisted
+        div.divisionset = None
         return div
 
-    def add_division(self, line):
-        line['Name'] = line['Name'].replace("’", "'")
-        org = self.get_org_from_line(line)
-        div_set = self.get_div_set(org, line)
-        div_identifier = self.get_identifier_from_line(div_set, line)
-        self.create_div_from_line(div_set, div_identifier, line)
+    def create_divisions(self, csv_data):
+        for line in csv_data:
+            line['Name'] = line['Name'].replace("’", "'")
+            org = self.get_org_from_line(line)
+            div_set = self.division_sets[org.official_identifier]
+            div_identifier = self.get_identifier_from_line(div_set, line)
+            division = self.create_div_from_line(org, div_identifier, line)
+            self.divisions.append(division)
+
+    @transaction.atomic
+    def save_all(self):
+        for _, record in self.division_sets.items():
+            record.save()
+        for record in self.divisions:
+            org = self.division_sets[record.organisation.official_identifier]
+            record.divisionset = org
+            record.save()
