cabb283d866d4fbbf0c3dc1ab21a143a6d32a1b2
chris48s <chris.shaw480@gmail.com>
2018-03-09 13:41:41 +0000

    allow meta data to be added and edited via /admin

--- every_election/apps/elections/admin.py
+++ every_election/apps/elections/admin.py
@@ -1,6 +1,10 @@
+import json
+from copy import deepcopy
 from django.contrib import admin
+from django.forms.widgets import Textarea
 from django_markdown.admin import MarkdownModelAdmin
-from .models import Election, Explanation
+from .models import Election, Explanation, MetaData
+
 
 class ElectionAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
@@ -23,5 +27,26 @@ class ElectionAdmin(admin.ModelAdmin):
         'geography',
     )
 
+
+class JSONEditor(Textarea):
+    def render(self, name, value, attrs=None):
+        # if its valid json, pretty print it
+        # if not (e.g: on init, validation error)
+        # just use the input string
+        try:
+            value = json.dumps(json.loads(value), sort_keys=True, indent=4)
+        finally:
+            return super().render(name, value, attrs)
+
+class MetaDataAdmin(admin.ModelAdmin):
+
+    def get_form(self, *args, **kwargs):
+        self.form = deepcopy(self.form)
+        form = super().get_form(*args, **kwargs)
+        form.base_fields['data'].widget = JSONEditor()
+        return form
+
+
 admin.site.register(Election, ElectionAdmin)
 admin.site.register(Explanation, MarkdownModelAdmin)
+admin.site.register(MetaData, MetaDataAdmin)
