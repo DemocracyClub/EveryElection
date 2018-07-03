0761308f6edf15bf5eb7df194cd6639bdc4c3fae
chris48s <chris.shaw480@gmail.com>
2018-07-03 18:31:20 +0100

    perform additional sanity check before declaring a match
    because there's always more worms in the can

--- every_election/apps/organisations/boundaryline/boundaryline.py
+++ every_election/apps/organisations/boundaryline/boundaryline.py
@@ -2,6 +2,18 @@ from django.contrib.gis.gdal import DataSource, OGRGeometry
 from django.utils.text import slugify
 
 
+# Percentage overlap required for us to consider 2 divisions
+# 'the same' without manual investigation
+SANITY_CHECK_TOLERANCE = 97
+
+
+def overlap_percent(geom1, geom2):
+    g1 = geom1.transform(27700, clone=True)
+    g2 = geom2.transform(27700, clone=True)
+    intersection = g1.intersection(g2)
+    return (intersection.area/g1.area)*100
+
+
 class BoundaryLine:
 
     def __init__(self, filename):
@@ -31,6 +43,33 @@ class BoundaryLine:
             )
         return 'gss:' + feature.get('code')
 
+    def get_match_warning(self, div, match):
+        # return a warning if there is something to warn about
+        # or None if everything looks OK
+
+        if not div.geography.geography:
+            # If we haven't got a division geography to check against,
+            # just assume its fine. Its probably fine.
+            return None
+
+        overlap = overlap_percent(
+            OGRGeometry(div.geography.geography.ewkt),
+            match.geom
+        )
+        if overlap >= SANITY_CHECK_TOLERANCE:
+            # close enough
+            return None
+
+        warning = "Found {code} as potential match for {div} " +\
+            "but BoundaryLine shape for {code} only covers {percent:.2f}% " +\
+            "of {div}'s area. Manual review required."
+        warning = warning.format(
+            code=self.get_code_from_feature(match),
+            div=div.official_identifier,
+            percent=overlap
+        )
+        return warning
+
     def get_division_code(self, div, org):
         filter_geom = OGRGeometry(org.geography.ewkt).transform(27700, clone=True)
         self.layer.spatial_filter = filter_geom
@@ -43,7 +82,7 @@ class BoundaryLine:
         match = None
         for feature in self.layer:
             if self.normalize_name(feature.get('name')) == division_name:
-                match = self.get_code_from_feature(feature)
+                match = feature
                 matches = matches + 1
             if matches > 1:
                 # ...but we also need to be a little bit careful
@@ -51,4 +90,12 @@ class BoundaryLine:
                     div=div.official_identifier))
                 return None
 
-        return match
+        if matches == 0:
+            return None
+
+        warning = self.get_match_warning(div, match)
+        if warning:
+            print(warning)
+            return None
+
+        return self.get_code_from_feature(match)
