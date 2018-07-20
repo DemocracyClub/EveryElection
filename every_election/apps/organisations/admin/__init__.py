dff5f8eb8d4947d7e0a9697f65d7b0297e77d3dc
chris48s <chris.shaw480@gmail.com>
2018-07-20 12:28:39 +0100

    expose DivisionSets via /admin

--- every_election/apps/organisations/admin/__init__.py
+++ every_election/apps/organisations/admin/__init__.py
@@ -2,13 +2,16 @@ from django.contrib import admin
 from organisations.models import (
     Organisation,
     OrganisationDivision,
+    OrganisationDivisionSet,
     OrganisationGeography
 )
 from .organisation import OrganisationAdmin
 from .organisation_division import OrganisationDivisionAdmin
+from .organisation_divisionset import OrganisationDivisionSetAdmin
 from .organisation_geography import OrganisationGeographyAdmin
 
 
 admin.site.register(Organisation, OrganisationAdmin)
 admin.site.register(OrganisationDivision, OrganisationDivisionAdmin)
+admin.site.register(OrganisationDivisionSet, OrganisationDivisionSetAdmin)
 admin.site.register(OrganisationGeography, OrganisationGeographyAdmin)
