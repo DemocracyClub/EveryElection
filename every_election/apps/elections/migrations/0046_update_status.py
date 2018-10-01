cdfb6076bf325fc7106f1ade5aeda0dcb031f644
chris48s <chris.shaw480@gmail.com>
2018-10-01 16:18:03 +0100

    add data migration to set existing elections to approved

---
+++ every_election/apps/elections/migrations/0046_update_status.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('elections', '0045_auto_20181001_1437'),
+    ]
+
+    operations = [
+        # assume all elections that already exist are approved
+        migrations.RunSQL("""
+            UPDATE elections_election SET suggested_status='approved'
+        """,
+        reverse_sql="""
+            UPDATE elections_election SET suggested_status='suggested'
+        """),
+    ]
