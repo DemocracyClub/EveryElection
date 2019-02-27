e0d7d97e11be66e6a6b311c48919ed3ea4e9930c
Alex Wilson <alex+github@probablyfine.co.uk>
2019-02-27 14:40:36 +0000

    tests: Add more tests to cover NI and Scottish locals.

--- packages/uk-election-timetables/tests/test_sopn_publish_date.py
+++ packages/uk-election-timetables/tests/test_sopn_publish_date.py
@@ -1,9 +1,9 @@
-from sopn_publish_date import sopn_publish_date_for_id
+from sopn_publish_date import sopn_publish_date_for_id, sopn_publish_date
 from datetime import datetime
 from pytest import fail
 
 
-def test_publish_date_english_local():
+def test_publish_date_local_group():
     try:
         sopn_publish_date_for_id('local.2019-02-21')
         fail("Should have thrown exception")
@@ -11,13 +11,20 @@ def test_publish_date_english_local():
         assert_exception_has_id(e, 'local.2019-02-21')
 
 
-def test_publish_date_parliamentary():
-
+def test_publish_date_parliamentary_group():
     try:
         sopn_publish_date_for_id('parl.2017-06-08')
         fail("Should have thrown exception")
     except Exception as e:
         assert_exception_has_id(e, 'parl.2017-06-08')
+
+
+def test_publish_date_mayoral_group():
+    try:
+        sopn_publish_date_for_id('mayor.2016-05-05')
+        fail("Should have thrown exception")
+    except Exception as e:
+        assert_exception_has_id(e, 'mayor.2016-05-05')
 
 
 # Reference election: sp.c.shetland-islands.2016-05-05
@@ -41,7 +48,31 @@ def test_publish_date_greater_london_assembly():
 
     publish_date = sopn_publish_date_for_id('gla.c.barnet-and-camden.2016-05-05')
 
-    assert publish_date == datetime(2016, 4, 1)
+    assert publish_date == datetime(2016, 3, 31)
+
+
+# Reference election: nia.belfast-east.2017-03-02
+def test_publish_date_northern_irish_assembly():
+
+    publish_date = sopn_publish_date_for_id('nia.belfast-east.2017-03-02')
+
+    assert publish_date == datetime(2017, 2, 8)
+
+
+# Reference election: local.highland.wester-ross-strathpeffer-and-lochalsh.by.2018-12-06
+def test_publish_date_scottish_local():
+
+    publish_date = sopn_publish_date('scotland', datetime(2018, 12, 6))
+
+    assert publish_date == datetime(2018, 11, 2)
+
+
+# Reference election: local.belfast.balmoral.2019-05-02
+def test_publish_date_northern_ireland_local():
+
+    publish_date = sopn_publish_date('northern-ireland', datetime(2019, 5, 2))
+
+    assert publish_date == datetime(2019, 4, 8)
 
 
 def assert_exception_has_id(exception, election_id):
