e8549107265076c1671b0796be9f95539379dad5
Alex Wilson <alex+github@probablyfine.co.uk>
2019-02-27 12:34:33 +0000

    feat(implementation): Throw exception for ambiguous ids
    * Create internal function that takes (country, poll date) for unambiguous election types such as nia, nwa, sp, and gla.
    * Throw exception for ambiguous types such as local, parl

--- packages/uk-election-timetables/tests/test_sopn_publish_date.py
+++ packages/uk-election-timetables/tests/test_sopn_publish_date.py
@@ -1,27 +1,48 @@
-from sopn_publish_date import sopn_publish_date
+from sopn_publish_date import sopn_publish_date_for_id
 from datetime import datetime
+from pytest import fail
 
 
-# Reference election:
 def test_publish_date_english_local():
+    try:
+        sopn_publish_date_for_id('local.2019-02-21')
+        fail("Should have thrown exception")
+    except Exception as e:
+        assert_exception_has_id(e, 'local.2019-02-21')
 
-    publish_date = sopn_publish_date('local.cardiff.2019-02-21')
 
-    assert publish_date == datetime(2019, 1, 25)
-
-
-# Reference election: parl.islington-north.2017-06-08
 def test_publish_date_parliamentary():
 
-    publish_date = sopn_publish_date('parl.islington-north.2017-06-08')
-
-    assert publish_date == datetime(2017, 5, 11)
+    try:
+        sopn_publish_date_for_id('parl.2017-06-08')
+        fail("Should have thrown exception")
+    except Exception as e:
+        assert_exception_has_id(e, 'parl.2017-06-08')
 
 
 # Reference election: sp.c.shetland-islands.2016-05-05
 def test_publish_date_scottish_parliament():
 
-    publish_date = sopn_publish_date('sp.c.shetland-islands.2016-05-05')
+    publish_date = sopn_publish_date_for_id('sp.c.shetland-islands.2016-05-05')
 
     assert publish_date == datetime(2016, 4, 1)
 
+
+# Reference election: naw.c.ceredigion.2016-05-05
+def test_publish_date_national_assembly_of_wales():
+
+    publish_date = sopn_publish_date_for_id('naw.c.ceredigion.2016-05-05')
+
+    assert publish_date == datetime(2016, 4, 7)
+
+
+# Reference election: gla.c.barnet-and-camden.2016-05-05
+def test_publish_date_greater_london_assembly():
+
+    publish_date = sopn_publish_date_for_id('gla.c.barnet-and-camden.2016-05-05')
+
+    assert publish_date == datetime(2016, 4, 1)
+
+
+def assert_exception_has_id(exception, election_id):
+    assert str(exception) == 'Cannot derive country from ambiguous election id [%s]' % election_id
