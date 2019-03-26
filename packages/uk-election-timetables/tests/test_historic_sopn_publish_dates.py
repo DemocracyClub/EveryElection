821d91b8000b140e1f9377f3aa72c88ba9c89d59
Alex Wilson <alex+github@probablyfine.co.uk>
2019-03-26 13:30:23 +0000

    test(historic): Set up data-driven tests from historic SoPNs

---
+++ packages/uk-election-timetables/tests/test_historic_sopn_publish_dates.py
@@ -0,0 +1,65 @@
+from sopn_publish_date import StatementPublishDate
+from datetime import timedelta, datetime
+from csv import DictReader
+from pytest import mark
+
+sopn_publish_date = StatementPublishDate()
+
+with open("./tests/historic_sopn_data.csv") as f:
+    historic_data = list(DictReader(f))
+
+
+def read_date(date_as_string):
+    return datetime.strptime(date_as_string, "%Y-%m-%d")
+
+
+def same_or_next_day(actual_date, expected_date):
+    return actual_date == expected_date or actual_date == (
+        expected_date + timedelta(days=1)
+    )
+
+
+def within_one_day(actual_date, expected_date):
+    return same_or_next_day(actual_date, expected_date) or actual_date == (
+        expected_date - timedelta(days=1)
+    )
+
+
+def generate_test_id(val):
+    return "%s-%s-%s" % (val["election_id"], val["geo_id"], val["sopn_id"])
+
+
+def generate_test_cases(type):
+    return [row for row in historic_data if type in row["election_id"]]
+
+
+@mark.parametrize("row", generate_test_cases("nia"), ids=generate_test_id)
+def test_nia(row):
+    assert same_or_next_day(
+        read_date(row["sopn_publish_date"]),
+        sopn_publish_date.for_id(row["election_id"]),
+    )
+
+
+@mark.parametrize("row", generate_test_cases("sp"), ids=generate_test_id)
+def test_sp(row):
+    assert same_or_next_day(
+        read_date(row["sopn_publish_date"]),
+        sopn_publish_date.for_id(row["election_id"]),
+    )
+
+
+@mark.parametrize("row", generate_test_cases("naw"), ids=generate_test_id)
+def test_naw(row):
+    assert same_or_next_day(
+        read_date(row["sopn_publish_date"]),
+        sopn_publish_date.for_id(row["election_id"]),
+    )
+
+
+@mark.parametrize("row", generate_test_cases("pcc"), ids=generate_test_id)
+def test_pcc(row):
+    assert within_one_day(
+        read_date(row["sopn_publish_date"]),
+        sopn_publish_date.for_id(row["election_id"]),
+    )
