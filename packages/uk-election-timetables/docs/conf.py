03b3fb40255dd2810f2cc20e2ed5816a0aaa5903
Alex Wilson <alex+github@probablyfine.co.uk>
2019-05-10 16:07:28 +0100

    chore(version): Bump to version 1.2.0

--- packages/uk-election-timetables/docs/conf.py
+++ packages/uk-election-timetables/docs/conf.py
@@ -5,9 +5,9 @@ copyright = '2019, Alex Wilson'
 author = 'Alex Wilson'
 
 # The short X.Y version
-version = '1.1'
+version = '1.2'
 # The full version, including alpha/beta/rc tags
-release = '1.1.1'
+release = '1.2.0'
 
 
 # -- General configuration ---------------------------------------------------
