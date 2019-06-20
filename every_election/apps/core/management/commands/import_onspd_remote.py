2e535d26baf5b4c2e72868d4a57193553f60d5b2
chris48s <chris.shaw480@gmail.com>
2019-06-20 14:53:10 +0100

    convert vars used in every function to object properties

--- every_election/apps/core/management/commands/import_onspd_remote.py
+++ every_election/apps/core/management/commands/import_onspd_remote.py
@@ -13,6 +13,12 @@ from uk_geo_utils.helpers import get_onspd_model
 
 
 class Command(BaseCommand):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.cursor = connection.cursor()
+        self.table_name = get_onspd_model()._meta.db_table
+        self.temp_table_name = self.table_name + "_temp"
+
     def add_arguments(self, parser):
         parser.add_argument("url", action="store")
 
@@ -27,13 +33,12 @@ class Command(BaseCommand):
         gb = ((mem.total / 1024) / 1024) / 1024
         return gb >= 2
 
-    def get_index_statements(self, table_name, temp_table_name):
-        cursor = connection.cursor()
-        cursor.execute(
+    def get_index_statements(self):
+        self.cursor.execute(
             "SELECT indexdef FROM pg_indexes WHERE tablename='%s' ORDER BY indexname LIKE '%%_pk' DESC;"
-            % (table_name)
+            % (self.table_name)
         )
-        results = cursor.fetchall()
+        results = self.cursor.fetchall()
 
         indexes = []
         statements = [row[0] for row in results]
@@ -49,17 +54,19 @@ class Command(BaseCommand):
         for index in indexes:
             index["temp_index_create_statement"] = index[
                 "original_index_create_statement"
-            ].replace(table_name, temp_table_name)
+            ].replace(self.table_name, self.temp_table_name)
             parsed_sql = sqlparse.parse(index["original_index_create_statement"])[0]
             identifiers = [
                 token.value
                 for token in parsed_sql.tokens
-                if not token.ttype and table_name in token.value
+                if not token.ttype and self.table_name in token.value
             ]
             if len(identifiers) != 2:
                 raise Exception("Expected 2 identifiers, found %i" % len(identifiers))
             original_index_name = identifiers[0]
-            temp_index_name = original_index_name.replace(table_name, temp_table_name)
+            temp_index_name = original_index_name.replace(
+                self.table_name, self.temp_table_name
+            )
             index["index_rename_statement"] = "ALTER INDEX %s RENAME TO %s" % (
                 temp_index_name,
                 original_index_name,
@@ -67,18 +74,18 @@ class Command(BaseCommand):
 
         return indexes
 
-    def create_temp_table(self, table_name, temp_table_name):
-        cursor = connection.cursor()
-        cursor.execute("DROP TABLE IF EXISTS %s;" % (temp_table_name))
-        cursor.execute(
+    def create_temp_table(self):
+        self.cursor.execute("DROP TABLE IF EXISTS %s;" % (self.temp_table_name))
+        self.cursor.execute(
             "CREATE TABLE %s AS SELECT * FROM %s LIMIT 0;"
-            % (temp_table_name, table_name)
+            % (self.temp_table_name, self.table_name)
         )
 
-    def swap_tables(self, table_name, temp_table_name):
-        cursor = connection.cursor()
-        cursor.execute("DROP TABLE %s;" % (table_name))
-        cursor.execute("ALTER TABLE %s RENAME TO %s;" % (temp_table_name, table_name))
+    def swap_tables(self):
+        self.cursor.execute("DROP TABLE %s;" % (self.table_name))
+        self.cursor.execute(
+            "ALTER TABLE %s RENAME TO %s;" % (self.temp_table_name, self.table_name)
+        )
 
     def handle(self, **options):
         if not self.check_memory():
@@ -93,17 +100,13 @@ class Command(BaseCommand):
         tempdir = unzip(tmp.name)
         data_path = os.path.join(tempdir, "Data")
 
-        table_name = get_onspd_model()._meta.db_table
-        temp_table_name = table_name + "_temp"
-        cursor = connection.cursor()
-
         try:
             self.stdout.write("Creating temp table..")
-            self.create_temp_table(table_name, temp_table_name)
+            self.create_temp_table()
 
             # import ONSPD into the temp table
             cmd = LocalImporter()
-            cmd.table_name = temp_table_name
+            cmd.table_name = self.temp_table_name
             cmd.path = data_path
             cmd.import_onspd()
 
@@ -112,25 +115,25 @@ class Command(BaseCommand):
             # new table with the exact names django expects them to have
             # (e.g: uk_geo_utils_onspd_pcds_9d376544_uniq )
             # so we can still run migrations and stuff on it
-            indexes = self.get_index_statements(table_name, temp_table_name)
+            indexes = self.get_index_statements()
 
             self.stdout.write("Building indexes..")
             for index in indexes:
-                cursor.execute(index["temp_index_create_statement"])
+                self.cursor.execute(index["temp_index_create_statement"])
 
             # drop the old table, swap in the new one and rename the indexes
             # do this bit in a transaction so if it fails
             # we don't leave ourselves in an inconsistent state
             with transaction.atomic():
                 self.stdout.write("Swapping tables..")
-                self.swap_tables(table_name, temp_table_name)
+                self.swap_tables()
 
                 self.stdout.write("Renaming indexes..")
                 for index in indexes:
-                    cursor.execute(index["index_rename_statement"])
+                    self.cursor.execute(index["index_rename_statement"])
 
         finally:
-            cursor.execute("DROP TABLE IF EXISTS %s;" % (temp_table_name))
+            self.cursor.execute("DROP TABLE IF EXISTS %s;" % (self.temp_table_name))
             self.stdout.write("Cleaning up temp files..")
             self.cleanup(tempdir)
 
