5d08f326987ed6cd196ee61dc33601be1c223d7b
Alex Wilson <alex+github@probablyfine.co.uk>
2019-08-24 12:24:19 +0100

    feat(for_id): Return None for europarl elections.
    * There's no clear way to derive Region from Country if the election is in England.

--- packages/uk-election-timetables/tests/test_sopn_publish_date.py
+++ packages/uk-election-timetables/tests/test_sopn_publish_date.py
@@ -32,6 +32,11 @@ def test_publish_date_parl_id_with_country():
     assert publish_date == date(2019, 1, 25)
 
 
+def test_publish_date_europarl_id_with_country():
+    publish_date = sopn_publish_date.for_id("europarl.2019-02-21", country=Country.ENGLAND)
+
+    assert publish_date is None
+
 def test_publish_date_not_an_election_type():
 
     with raises(NoSuchElectionTypeError) as err:
