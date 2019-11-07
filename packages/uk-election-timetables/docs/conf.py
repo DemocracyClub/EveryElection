aa0554634120b5e21db9f209d975638c1af18517
Alex Wilson <gpg@probablyfine.co.uk>
2019-11-07 19:01:11 +0000

    chore(version): Bump to version 1.3.2
    * Fixed issue with uk_parliament calculations
    * Fixed issue with calendar dates being propagated to the wrong country

--- packages/uk-election-timetables/docs/conf.py
+++ packages/uk-election-timetables/docs/conf.py
@@ -7,7 +7,7 @@ author = 'Alex Wilson'
 # The short X.Y version
 version = '1.3'
 # The full version, including alpha/beta/rc tags
-release = '1.3.1'
+release = '1.3.2'
 
 
 # -- General configuration ---------------------------------------------------
