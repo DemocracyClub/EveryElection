a4914f1309ae8fb0e81a323f99ca6b657c5f163c
Alex Wilson <gpg@probablyfine.co.uk>
2020-03-01 16:20:12 +0000

    feat(date): Support subtraction of days from initial date

--- packages/uk-election-timetables/tests/test_date.py
+++ packages/uk-election-timetables/tests/test_date.py
@@ -6,3 +6,10 @@ def test_zero_days_before():
     example = date(2020, 1, 1)
 
     assert days_before(example, 0) == example
+
+
+def test_non_zero_days_before():
+    example = date(2020, 1, 1)
+
+    assert days_before(example, 1) == date(2019, 12, 31)
+    assert days_before(example, 2) == date(2019, 12, 30)
