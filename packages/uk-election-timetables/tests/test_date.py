c8b3710329f37b59cf39d569410709a5d4fa319d
Alex Wilson <gpg@probablyfine.co.uk>
2020-03-01 16:23:01 +0000

    feat(date): Ignore weekends in calculation

--- packages/uk-election-timetables/tests/test_date.py
+++ packages/uk-election-timetables/tests/test_date.py
@@ -13,3 +13,9 @@ def test_non_zero_days_before():
 
     assert days_before(example, 1) == date(2019, 12, 31)
     assert days_before(example, 2) == date(2019, 12, 30)
+
+
+def test_ignore_weekends():
+    example = date(2020, 1, 6)  # Monday
+
+    assert days_before(example, 1) == date(2020, 1, 3)
