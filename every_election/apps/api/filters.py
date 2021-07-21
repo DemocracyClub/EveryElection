dedb85128654d516d8315b642e456ba0cddd1c47
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2021-07-21 12:56:48 +0100

    Add filters for organisation type, start_date

--- every_election/apps/api/filters.py
+++ every_election/apps/api/filters.py
@@ -9,6 +9,14 @@ class ElectionFilter(django_filters.FilterSet):
         field_name="organisation__official_identifier",
         lookup_expr="exact",
     )
+    organisation_type = django_filters.CharFilter(
+        field_name="organisation__organisation_type",
+        lookup_expr="exact",
+    )
+    organisation_start_date = django_filters.DateFilter(
+        field_name="organisation__start_date",
+        lookup_expr="exact",
+    )
 
     class Meta:
         model = Election
