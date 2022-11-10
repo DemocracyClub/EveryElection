7b8292296cf074ea37813b307d383f799a3f7673
Sym Roe <sym.roe@talusdesign.co.uk>
2022-11-10 11:19:10 +0000

    Initial CDK image builder

---
+++ cdk_app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import os
+
+from aws_cdk import core as cdk
+
+from cdk_imagebuilder.stacks.image_builder import EEImageUpdater
+
+app = cdk.App()
+
+EEImageUpdater(
+    app,
+    "EEImageUpdater",
+    env=cdk.Environment(
+        account=os.getenv("CDK_DEFAULT_ACCOUNT"), region="eu-west-2"
+    ),
+)
+
+
+app.synth()
