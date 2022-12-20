0ae8d235a2686c377701c8cff40c4a01c33b0774
Sym Roe <sym.roe@talusdesign.co.uk>
2022-12-20 12:32:38 +0000

    Pass DC Env in to stacks

--- cdk_app.py
+++ cdk_app.py
@@ -7,9 +7,26 @@ from aws_cdk.core import Tags
 from cdk_imagebuilder.stacks.code_deploy import EECodeDeployment
 from cdk_imagebuilder.stacks.image_builder import EEImageUpdater
 
-app = cdk.App()
+valid_environments = (
+    "development",
+    "staging",
+    "production",
+)
+
+app_wide_context = {}
+if dc_env := os.environ.get("DC_ENVIRONMENT"):
+    app_wide_context["dc-environment"] = dc_env
+
+app = cdk.App(context=app_wide_context)
 
 env = cdk.Environment(account=os.getenv("CDK_DEFAULT_ACCOUNT"), region="eu-west-2")
+
+# Set the DC Environment early on. This is important to be able to conditionally
+# change the stack configurations
+dc_environment = app.node.try_get_context("dc-environment") or None
+assert (
+    dc_environment in valid_environments
+), f"context `dc-environment` must be one of {valid_environments}"
 
 EEImageUpdater(
     app,
