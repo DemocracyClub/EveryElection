e8a12ec527bb95a1db258268ed4e6ec5b059922c
Sym Roe <sym.roe@talusdesign.co.uk>
2023-02-02 10:26:12 +0000

    Bind Gunicorn to all IPv4 interfaces

--- deployscripts/start_application.sh
+++ deployscripts/start_application.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -xeE
 
+# Ensure gunicorn is on the right port
+sudo sed "s/127.0.0.1/0.0.0.0/g" /etc/systemd/system/gunicorn_every_election.service -i
+sudo systemctl daemon-reload
+
 # enabling will allow the services to start if the instance reboots
 systemctl enable gunicorn_every_election.service
-
 systemctl start gunicorn_every_election.service
