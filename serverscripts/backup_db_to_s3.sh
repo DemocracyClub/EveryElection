173da6259b3d87ceb78e9e44001abfa08e6dc226
Sym Roe <sym.roe@talusdesign.co.uk>
2023-02-06 13:08:01 +0000

    Don't pipe directly to S3
    The S3 pipe is handy, but the pipe is opened _before_ the Postgres
    connection (this is a UNIX thing). This means that if anything goes
    wrong with the connection or the dump, the pipe ends up writing an empty
    file to S3.
    
    Later, this breaks any script that uses `pg_restore` on the empty file.

--- serverscripts/backup_db_to_s3.sh
+++ serverscripts/backup_db_to_s3.sh
@@ -18,15 +18,19 @@ SHORT_TERM_BUCKET='dc-ee-production-database-backups'
 BACKUP_PG_DUMP_CONNECTION_STRING="-d every_election -h $EE_DATABASE_HOST -Fc"
 export PGPASSWORD=$EE_DATABASE_PASSWORD
 
+pg_dump $BACKUP_PG_DUMP_CONNECTION_STRING > /tmp/ee-backup.dump
+
 if  [ $DAY_OF_MONTH = 01 ] ;
 then
-  pg_dump $BACKUP_PG_DUMP_CONNECTION_STRING | aws s3 cp - \
+   aws s3 cp /tmp/ee-backup.dump \
     s3://$SHORT_TERM_BUCKET/$DESTDIR/$NOWDATE-backup.dump \
     --storage-class=STANDARD_IA
 
 else
-  pg_dump $BACKUP_PG_DUMP_CONNECTION_STRING | aws s3 cp - \
+  aws s3 cp /tmp/ee-backup.dump \
     s3://$SHORT_TERM_BUCKET/$DESTDIR/$NOWDATE-backup.dump \
     --storage-class=STANDARD_IA \
     --expires "$(date -I -d '60 days')"
 fi
+
+rm /tmp/ee-backup.dump
