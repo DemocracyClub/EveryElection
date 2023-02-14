6598b4028924576a5c71f96ca30ef2dd599df4e3
Beka Lewis <bekabyx@gmail.com>
2023-02-14 15:08:41 +0000

    Bump version to 2.4.0

--- packages/uk-election-timetables/docs/conf.py
+++ packages/uk-election-timetables/docs/conf.py
@@ -10,9 +10,9 @@ copyright = "2020, Alex Wilson"
 author = "Alex Wilson"
 
 # The short X.Y version
-version = "2.3"
+version = "2.4"
 # The full version, including alpha/beta/rc tags
-release = "2.3.0"
+release = "2.4.0"
 
 
 # -- General configuration ---------------------------------------------------
