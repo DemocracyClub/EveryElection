789a7dcac9b5b09ca7273caecb7976e154020934
Sym Roe <sym.roe@talusdesign.co.uk>
2023-02-28 11:04:56 +0000

    Fix double quoting in cron script

--- serverscripts/add_nuts1.sh
+++ serverscripts/add_nuts1.sh
@@ -6,5 +6,5 @@ cd /var/www/every_election/repo/
 source .venv/bin/activate
 python manage.py add_tags \
     -u "https://ons-cache.s3.eu-west-1.amazonaws.com/NUTS_Level_1_(January_2018)_Boundaries.geojson" \
-    --fields ''{"NUTS118NM": "value", "NUTS118CD": "key"}'' \
+    --fields '{"NUTS118NM": "value", "NUTS118CD": "key"}' \
     --tag-name NUTS1
