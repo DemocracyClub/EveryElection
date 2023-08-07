229dcd1a9b8f4bfa7c51c17cb42ea72711616504
Sym Roe <sym.roe@talusdesign.co.uk>
2023-08-07 18:26:52 +0100

    Clean up post to Slack logic

--- every_election/apps/election_snooper/helpers.py
+++ every_election/apps/election_snooper/helpers.py
@@ -8,23 +8,18 @@ from django.conf import settings
 
 
 def post_to_slack(message):
+    if not getattr(settings, "SLACK_WEBHOOK_URL", None):
+        warnings.warn("settings.SLACK_WEBHOOK_URL is not set")
+        return
+
     env = os.getenv(
         "SERVER_ENVIRONMENT", getattr(settings, "SERVER_ENVIRONMENT", None)
     )
     if env in ["test", "development", "staging"]:
         prefix = "TEST for {} environment: ".format(env)
         message = prefix + message
-    url = settings.SLACK_WEBHOOK_URL
 
-    if not hasattr(settings, "SLACK_WEBHOOK_URL") and env == "production":
-        warnings.warn("settings.SLACK_WEBHOOK_URL is not set")
-        return
-    if not hasattr(settings, "SLACK_WEBHOOK_URL") and env in [
-        "test",
-        "development",
-        "staging",
-    ]:
-        print("SLACK_WEBHOOK_URL is not set")
+    url = settings.SLACK_WEBHOOK_URL
 
     payload = {
         "icon_emoji": ":satellite_antenna:",
