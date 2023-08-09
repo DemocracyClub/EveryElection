cf005cce094064bc13fb5fc97bf567e7c2beb91d
Sym Roe <sym.roe@talusdesign.co.uk>
2023-08-09 08:17:19 +0100

    Check if user is authenticated before checking their groups

--- every_election/apps/core/helpers.py
+++ every_election/apps/core/helpers.py
@@ -1,6 +1,8 @@
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
 
 
-def user_is_moderator(user):
+def user_is_moderator(user: User):
+    if not user.is_authenticated:
+        return False
     group = Group.objects.get(name="moderators")
     return group in user.groups.all()
