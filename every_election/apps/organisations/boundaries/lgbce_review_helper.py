1ecd7d0ccb84dbfab97cce3e16a1772ca062ac0d
GeoWill <roper.william@gmail.com>
2023-12-01 00:34:59 +0000

    LGBCE review helper
    The new class contains the functionality to create the CSVs that we need
    for creating the new divsets from LGBCE boundary reviews.

---
+++ every_election/apps/organisations/boundaries/lgbce_review_helper.py
@@ -0,0 +1,259 @@
+import copy
+import csv
+import sys
+from datetime import datetime, timedelta
+from io import BytesIO, StringIO
+
+import boto3
+import botocore
+import requests
+from django.conf import settings
+from eco_parser import EcoParser, ParseError
+from organisations.models import OrganisationBoundaryReview
+
+
+def check_s3_obj_exists(s3_client: boto3.client, bucket: str, key: str):
+    try:
+        s3_client.head_object(Bucket=bucket, Key=key)
+        return True
+    except botocore.exceptions.ClientError as e:
+        if e.response["Error"]["Code"] == "404":
+            # The key does not exist.
+            return False
+
+        if e.response["Error"]["Code"] == 403:
+            # Unauthorized
+            sys.stderr.write(
+                "Access denied. Do you have the correct permissions?"
+            )
+            raise
+
+        if e.response["Error"]["Code"] == "NoSuchBucket":
+            # Bucket doesn't exist
+            raise
+        # Something else has gone wrong.
+        raise
+
+
+def upload_obj_from_url(
+    s3_client: boto3.client, url: str, bucket: str, key: str
+):
+    response = requests.get(url)
+    response.raise_for_status()
+    s3_client.put_object(Bucket=bucket, Key=key, Body=BytesIO(response.content))
+
+
+class MissingLegislationTitleError(ValueError):
+    pass
+
+
+class LGBCEReviewHelper:
+    """
+    Class to help upload csvs based on boundary reviews to s3
+    """
+
+    def __init__(self, overwrite: bool = False, stdout=sys.stdout):
+        self.s3_client = boto3.client("s3")
+        self.overwrite = overwrite
+        self.stdout = stdout
+        self.review_bucket = settings.LGBCE_BUCKET
+
+    def get_legislation_title_from_review(
+        self, review: OrganisationBoundaryReview
+    ):
+        if review.legislation_title:
+            return review
+
+        raise MissingLegislationTitleError(
+            f"{review} is missing Legislation Title"
+        )
+
+    def upload_boundaries_to_s3(self, review: OrganisationBoundaryReview):
+        if not review.boundaries_url:
+            self.stdout.write(f"No boundary_url found for {review}")
+            return
+        if (
+            check_s3_obj_exists(
+                self.s3_client, self.review_bucket, review.s3_boundaries_key
+            )
+            and not self.overwrite
+        ):
+            self.stdout.write(
+                f"{review.s3_boundaries_key} already exists. Perhaps you meant to initialise with 'overwrite=True'?"
+            )
+            return
+
+        self.stdout.write(
+            f"Uploading {review.boundaries_url} to s3://{self.review_bucket}/{review.s3_boundaries_key}"
+        )
+
+        boundaries_response = requests.get(review.lgbce_boundary_url)
+        boundaries_response.raise_for_status()
+        self.s3_client.put_object(
+            Bucket=self.review_bucket,
+            Key=review.s3_boundaries_key,
+            Body=BytesIO(boundaries_response.content),
+        )
+
+    def upload_end_date_csv_to_s3(
+        self,
+        review: OrganisationBoundaryReview,
+        start_date: str,
+        thursday_start_day: bool = True,
+    ):
+        def day_before(date_str: str):
+            date_object = datetime.strptime(date_str, "%Y-%m-%d")
+            previous_day = date_object - timedelta(days=1)
+            return previous_day.strftime("%Y-%m-%d")
+
+        def is_first_thursday_in_may(date_str: str):
+            date_object = datetime.strptime(date_str, "%Y-%m-%d")
+            return date_object.month == 5 and date_object.weekday() == 3
+
+        if not is_first_thursday_in_may(start_date) and not thursday_start_day:
+            self.stdout.write(
+                f"{start_date} is not the the first Thursday in May. Double check."
+            )
+            return
+
+        end_date_s3_key = f"{review.s3_directory_key}/end_date.csv"
+
+        if (
+            check_s3_obj_exists(
+                self.s3_client, self.review_bucket, end_date_s3_key
+            )
+            and not self.overwrite
+        ):
+            self.stdout.write(
+                f"{end_date_s3_key} already exists. Perhaps you meant to initialise with 'overwrite=True'?"
+            )
+            return
+
+        buffer = StringIO(newline="")
+        writer = csv.writer(buffer)
+        writer.writerow(["org", "start_date", "end_date"])
+        writer.writerow(
+            [
+                review.organisation.official_identifier,
+                start_date,
+                day_before(start_date),
+            ]
+        )
+        csv_bytes = bytes(
+            buffer.getvalue().replace("\r\n", "\n"), encoding="utf-8"
+        )
+        self.stdout.write(
+            f"Uploading end_date_csv to s3://{self.review_bucket}/{review.s3_directory_key}/end_date.csv"
+        )
+        self.s3_client.put_object(
+            Bucket=self.review_bucket,
+            Key=end_date_s3_key,
+            Body=csv_bytes,
+        )
+
+    def get_xml_link_from_eco_url(self, eco_url: str):
+        """
+        There's an xml data page hiding somewhere
+        """
+
+        parts = ["schedule/1", "schedule", "schedules"]
+        for part in parts:
+            xml_link = f"{eco_url}/{part}/made/data.xml"
+            response = requests.head(xml_link, allow_redirects=True)
+            if response.status_code == 200:
+                return xml_link
+
+        self.stdout.write(f"No xml data link found for {eco_url}")
+        return None
+
+    def parse_eco_xml(self, xml_link: str):
+        eco_parser = EcoParser(xml_link)
+        try:
+            return eco_parser.parse()
+        except ParseError:
+            self.stdout.write(f"Could not parse eco at {xml_link}")
+        return None
+
+    def get_seats_total_from_leg_text(self, review: OrganisationBoundaryReview):
+        """
+        If they all have the same number of seats the count isn't included in the ward data :/
+        Here are some examples of what the text looks like
+
+        https://www.legislation.gov.uk/uksi/2021/108/made
+            '(4) The number of councillors to be elected for each ward is three. '
+
+        https://www.legislation.gov.uk/uksi/2022/967/made
+            '(4) Three councillors are to be elected for each ward. '
+
+        https://www.legislation.gov.uk/uksi/2022/1373/made
+            '(4) The number of councillors to be elected for each ward is two. '
+
+        https://www.legislation.gov.uk/uksi/2023/1205/article/3/made
+            '(4) The number of councillors to be elected for each ward is the number specified in relation to that ward in the second column of the table in Schedule 1.'
+
+        https://www.legislation.gov.uk/uksi/2015/70/made
+            '(4) The number of councillors to be elected for each district ward is the number specified in relation to that ward in the second column of the table in Schedule 1. '
+        """
+        pass  # ToDo
+
+    def get_base_eco_row(self, review: OrganisationBoundaryReview):
+        return {
+            "Start Date": "",
+            "End Date": "",
+            "Name": "",
+            "official_identifier": "",
+            "geography_curie": "",
+            "seats_total": "",
+            "Boundary Commission Consultation URL": review.consultation_url,
+            "Legislation URL": f"{review.cleaned_legislation_url}/made",
+            "Short Title": review.legislation_title,
+            "Notes": "",
+            "Mapit Generation URI": "",
+            "Organisation ID": review.organisation.official_identifier,
+            "Organisation ID type": "local-authority-eng",
+        }
+
+    def get_eco_csv_rows(self, review: OrganisationBoundaryReview):
+        xml_link = self.get_xml_link_from_eco_url(
+            review.cleaned_legislation_url
+        )
+        ward_data = self.parse_eco_xml(xml_link)
+        base_eco_row = self.get_base_eco_row(review)
+        if ward_data[len(ward_data) // 2] == 1:  # pick one from the middle
+            base_eco_row["seats_total"] = self.get_seats_total_from_leg_text(
+                review
+            )
+        wards = []
+        for ward in ward_data:
+            row = copy.deepcopy(base_eco_row)
+            if (
+                ward[0].startswith("(1)")
+                or ward[0].startswith("Column (")
+                or ward[0].startswith("Column 1")
+            ):
+                continue
+            row["Name"] = ward[0]
+
+            if len(ward) >= 2:
+                row["seats_total"] = ward[-1]
+            wards.append(row)
+        return wards
+
+    def make_eco_csv(self, review: OrganisationBoundaryReview):
+        ward_rows = self.get_eco_csv_rows(review)
+        buffer = StringIO(newline="")
+        writer = csv.DictWriter(buffer, fieldnames=ward_rows[0].keys())
+        writer.writeheader()
+        writer.writerows(ward_rows)
+        return bytes(buffer.getvalue().replace("\r\n", "\n"), encoding="utf-8")
+
+    def upload_eco_csv_to_s3(self, review: OrganisationBoundaryReview):
+        csv_bytes = self.make_eco_csv(review)
+        self.stdout.write(
+            f"Uploading end_date_csv to s3://{self.review_bucket}/{review.s3_directory_key}/eco.csv"
+        )
+        self.s3_client.put_object(
+            Bucket=self.review_bucket,
+            Key=f"{review.s3_directory_key}/eco.csv",
+            Body=csv_bytes,
+        )
---
+++ every_election/apps/organisations/boundaries/tests/test_lgbce_review_helper.py
@@ -0,0 +1,421 @@
+import os
+from io import StringIO
+from unittest.mock import Mock, patch
+
+import boto3
+from botocore.exceptions import ClientError
+from django.test import TestCase, override_settings
+from moto import mock_s3
+from organisations.boundaries.lgbce_review_helper import (
+    LGBCEReviewHelper,
+    check_s3_obj_exists,
+)
+from organisations.tests.factories import (
+    CompletedOrganisationBoundaryReviewFactory,
+    IncompleteOrganisationBoundaryReviewFactory,
+    OrganisationFactory,
+    UnprocessedOrganisationBoundaryReviewFactory,
+)
+
+TEST_LGBCE_MIRROR_BUCKET = "test-lgbce-mirror"
+
+
+def get_content_length(s3_client, bucket, key):
+    response = s3_client.head_object(Bucket=bucket, Key=key)
+    return int(response["ResponseMetadata"]["HTTPHeaders"]["content-length"])
+
+
+@mock_s3
+class TestLGBCEReviewHelper(TestCase):
+    def setUp(self):
+        # Don't do anything unintended
+        os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+        os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+        os.environ["AWS_SECURITY_TOKEN"] = "testing"
+        os.environ["AWS_SESSION_TOKEN"] = "testing"
+        os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
+
+        # Set up s3 client
+        self.s3 = boto3.client("s3", region_name="eu-west-2")
+
+        # Create mock bucket
+        conn = boto3.resource("s3", region_name="us-east-1")
+        conn.create_bucket(Bucket=TEST_LGBCE_MIRROR_BUCKET)
+
+        # Create fixture objects
+        self.unprocessed_review = UnprocessedOrganisationBoundaryReviewFactory()
+        self.processed_review = CompletedOrganisationBoundaryReviewFactory(
+            boundaries_url="path/to/processed_review_polys.zip"
+        )
+        self.incomplete_review = IncompleteOrganisationBoundaryReviewFactory()
+
+        # Create processed objects on s3
+        self.s3.put_object(
+            Bucket=TEST_LGBCE_MIRROR_BUCKET,
+            Key=self.processed_review.s3_boundaries_key,
+            Body=bytes("polygons!", encoding="utf-8"),
+        )
+        # Create processed objects on s3
+        self.s3.put_object(
+            Bucket=TEST_LGBCE_MIRROR_BUCKET,
+            Key=f"{self.processed_review.s3_directory_key}/end_date.csv",
+            Body=bytes(
+                "org,start_date,end_date\norg_identifier,2023-05-04,2023-05-03\n",
+                encoding="utf-8",
+            ),
+        )
+
+    def test_check_s3_obj_exists_exists(self):
+        self.assertTrue(
+            check_s3_obj_exists(
+                self.s3,
+                TEST_LGBCE_MIRROR_BUCKET,
+                self.processed_review.s3_boundaries_key,
+            )
+        )
+
+    def test_check_s3_obj_exists_not_exists(self):
+        self.assertFalse(
+            check_s3_obj_exists(
+                self.s3,
+                TEST_LGBCE_MIRROR_BUCKET,
+                "foobar",
+            )
+        )
+
+    def test_check_s3_obj_exists_no_bucket(self):
+        with self.assertRaises(ClientError) as e:
+            check_s3_obj_exists(
+                self.s3, "this-bucket-doesnt-exist-450928236839", "foobar"
+            )
+        self.assertEqual(e.exception.response["Error"]["Code"], "NoSuchBucket")
+
+    @override_settings(LGBCE_BUCKET=TEST_LGBCE_MIRROR_BUCKET)
+    def test_upload_boundaries_to_s3_already_exists(self):
+        buffer = StringIO()
+        lgbce_review_helper = LGBCEReviewHelper(stdout=buffer)
+        lgbce_review_helper.upload_boundaries_to_s3(self.processed_review)
+        self.assertEqual(
+            f"{self.processed_review.slug}/The {self.processed_review.organisation.common_name} (Electoral Changes) "
+            f"Order 2023/processed_review_polys.zip already exists. Perhaps you meant to initialise with "
+            f"'overwrite=True'?",
+            buffer.getvalue(),
+        )
+
+    @override_settings(LGBCE_BUCKET=TEST_LGBCE_MIRROR_BUCKET)
+    @patch("requests.get")
+    def test_upload_boundaries_to_s3(self, mock_get):
+        # This is mocking the boundaries that are downloaded from the lgbce site
+        mock_get.return_value = mock_response = Mock()
+        mock_response.status_code = 200
+        mock_response.content = b"Some polygons!!"
+
+        buffer = StringIO()
+        with self.assertRaises(ClientError) as e:
+            get_content_length(
+                self.s3,
+                bucket=TEST_LGBCE_MIRROR_BUCKET,
+                key=self.unprocessed_review.s3_boundaries_key,
+            )
+            self.assertEqual(e.exception.response["Code"], 404)
+
+        lgbce_review_helper = LGBCEReviewHelper(stdout=buffer, overwrite=True)
+        lgbce_review_helper.upload_boundaries_to_s3(self.processed_review)
+        self.assertEqual(
+            f"Uploading {self.processed_review.boundaries_url} to s3://{TEST_LGBCE_MIRROR_BUCKET}/{self.processed_review.s3_boundaries_key}",
+            buffer.getvalue(),
+        )
+        mock_get.assert_called_once_with(
+            "https://www.lgbce.org.uk/path/to/processed_review_polys.zip"
+        )
+        self.assertEqual(
+            15,
+            get_content_length(
+                self.s3,
+                bucket=TEST_LGBCE_MIRROR_BUCKET,
+                key=self.processed_review.s3_boundaries_key,
+            ),
+        )
+
+    @override_settings(LGBCE_BUCKET=TEST_LGBCE_MIRROR_BUCKET)
+    @patch("requests.get")
+    def test_upload_boundaries_to_s3_already_exists_overwrite(self, mock_get):
+        # This is mocking the boundaries that are downloaded from the lgbce site
+        mock_get.return_value = mock_response = Mock()
+        mock_response.status_code = 200
+        mock_response.content = b"Some different polygons"
+
+        buffer = StringIO()
+        self.assertEqual(
+            9,
+            get_content_length(
+                self.s3,
+                bucket=TEST_LGBCE_MIRROR_BUCKET,
+                key=self.processed_review.s3_boundaries_key,
+            ),
+        )
+        lgbce_review_helper = LGBCEReviewHelper(stdout=buffer, overwrite=True)
+        lgbce_review_helper.upload_boundaries_to_s3(self.processed_review)
+        self.assertEqual(
+            f"Uploading {self.processed_review.boundaries_url} to s3://{TEST_LGBCE_MIRROR_BUCKET}/{self.processed_review.s3_boundaries_key}",
+            buffer.getvalue(),
+        )
+        mock_get.assert_called_once_with(
+            "https://www.lgbce.org.uk/path/to/processed_review_polys.zip"
+        )
+        self.assertEqual(
+            23,
+            get_content_length(
+                self.s3,
+                bucket=TEST_LGBCE_MIRROR_BUCKET,
+                key=self.processed_review.s3_boundaries_key,
+            ),
+        )
+
+    @override_settings(LGBCE_BUCKET=TEST_LGBCE_MIRROR_BUCKET)
+    def test_upload_end_date_csv_to_s3_already_exists(self):
+        buffer = StringIO()
+        lgbce_review_helper = LGBCEReviewHelper(stdout=buffer)
+        lgbce_review_helper.upload_end_date_csv_to_s3(
+            self.processed_review, "2024-05-02"
+        )
+        self.assertEqual(
+            f"{self.processed_review.s3_directory_key}/end_date.csv already exists. Perhaps you meant to initialise "
+            f"with 'overwrite=True'?",
+            buffer.getvalue(),
+        )
+
+    @override_settings(LGBCE_BUCKET=TEST_LGBCE_MIRROR_BUCKET)
+    def test_upload_end_date_csv_to_s3(self):
+        with self.assertRaises(ClientError) as e:
+            get_content_length(
+                self.s3,
+                bucket=TEST_LGBCE_MIRROR_BUCKET,
+                key=f"{self.unprocessed_review.s3_directory_key}/end_date.csv",
+            )
+            self.assertEqual(e.exception.response["Code"], 404)
+
+        buffer = StringIO()
+        lgbce_review_helper = LGBCEReviewHelper(stdout=buffer)
+        lgbce_review_helper.upload_end_date_csv_to_s3(
+            self.unprocessed_review, "2024-05-02"
+        )
+
+        self.assertEqual(
+            f"Uploading end_date_csv to "
+            f"s3://{TEST_LGBCE_MIRROR_BUCKET}/{self.unprocessed_review.s3_directory_key}/end_date.csv",
+            buffer.getvalue(),
+        )
+        csv_body = self.s3.get_object(
+            Bucket=TEST_LGBCE_MIRROR_BUCKET,
+            Key=f"{self.unprocessed_review.s3_directory_key}/end_date.csv",
+        )["Body"].read()
+        self.assertEqual(
+            f"org,start_date,end_date\n"
+            f"{self.unprocessed_review.organisation.official_identifier},2024-05-02,2024-05-01\n",
+            csv_body.decode("utf-8"),
+        )
+
+    def test_get_xml_link_from_eco_url_a(self):
+        def get_responses(link, **kwargs):
+            """
+            The method being tested 'heads' the links generated to determine if they
+            return a 200. So we need to mock some respnses
+            """
+            mock = Mock()
+            tail = "/".join(link.split("/")[6:])
+            match tail:
+                case "schedule/1/made/data.xml":
+                    mock.status_code = 200
+                    return mock
+                case "schedule/made/data.xml":
+                    mock.status_code = 404
+                    return mock
+                case "schedules/made/data.xml":
+                    mock.status_code = 404
+                    return mock
+                case _:
+                    mock.status_code = 404
+                    return mock
+
+        with patch("requests.head", side_effect=get_responses) as mock_head:
+            mock_head.return_value = Mock(side_effect=get_responses)
+            lgbce_review_helper = LGBCEReviewHelper()
+            self.assertEqual(
+                "https://www.legislation.gov.uk/uksi/2017/1315/schedule/1/made/data.xml",
+                lgbce_review_helper.get_xml_link_from_eco_url(
+                    "https://www.legislation.gov.uk/uksi/2017/1315"
+                ),
+            )
+
+    def test_get_xml_link_from_eco_url_b(self):
+        def get_responses(link, **kwargs):
+            """
+            The method being tested 'heads' the links generated to determine if they
+            return a 200. So we need to mock some responses.
+            """
+            mock = Mock()
+            tail = "/".join(link.split("/")[6:])
+            match tail:
+                case "schedule/1/made/data.xml":
+                    mock.status_code = 404
+                    return mock
+                case "schedule/made/data.xml":
+                    mock.status_code = 200
+                    return mock
+                case "schedules/made/data.xml":
+                    mock.status_code = 404
+                    return mock
+                case _:
+                    mock.status_code = 404
+                    return mock
+
+        with patch("requests.head", side_effect=get_responses) as mock_head:
+            mock_head.return_value = Mock(side_effect=get_responses)
+            lgbce_review_helper = LGBCEReviewHelper()
+            self.assertEqual(
+                "https://www.legislation.gov.uk/uksi/2021/1053/schedule/made/data.xml",
+                lgbce_review_helper.get_xml_link_from_eco_url(
+                    "https://www.legislation.gov.uk/uksi/2021/1053"
+                ),
+            )
+
+    def test_get_xml_link_from_eco_url_c(self):
+        def get_responses(link, **kwargs):
+            """
+            The method being tested 'heads' the links generated to determine if they
+            return a 200. So we need to mock some responses.
+            """
+            mock = Mock()
+            tail = "/".join(link.split("/")[6:])
+            match tail:
+                case "schedule/1/made/data.xml":
+                    mock.status_code = 404
+                    return mock
+                case "schedule/made/data.xml":
+                    mock.status_code = 404
+                    return mock
+                case "schedules/made/data.xml":
+                    mock.status_code = 200
+                    return mock
+                case _:
+                    mock.status_code = 404
+                    return mock
+
+        with patch("requests.head", side_effect=get_responses) as mock_head:
+            mock_head.return_value = Mock(side_effect=get_responses)
+            lgbce_review_helper = LGBCEReviewHelper()
+            self.assertEqual(
+                "https://www.legislation.gov.uk/uksi/2021/417/schedules/made/data.xml",
+                lgbce_review_helper.get_xml_link_from_eco_url(
+                    "https://www.legislation.gov.uk/uksi/2021/417"
+                ),
+            )
+
+    @patch("eco_parser.parser.EcoParser.get_data")
+    def test_parse_eco_xml(self, get_data_mock):
+        with open(
+            "every_election/apps/organisations/boundaries/fixtures/buckinghamshire-eco-data.xml"
+        ) as f:
+            bucks_xml = f.read()
+        get_data_mock.return_value = bucks_xml
+        lgbce_review_helper = LGBCEReviewHelper()
+        expected = [
+            (
+                "(1) Name of Ward",
+                "(2) Number of councillors",
+            ),
+            ("Abbey", "2"),
+            ("Amersham & Chesham Bois", "3"),
+            ("Aston Clinton & Weston Turville", "2"),
+            ("Aylesbury East", "2"),
+            ("Aylesbury North", "2"),
+            ("Aylesbury North West", "2"),
+            ("Aylesbury South East", "2"),
+            ("Aylesbury South West", "2"),
+            ("Aylesbury West", "2"),
+            ("Beaconsfield", "2"),
+            ("Berryfields, Buckingham Park & Watermead", "2"),
+            ("Bierton, Kingsbrook & Wing", "2"),
+            ("Booker & Cressex", "1"),
+            ("Buckingham", "3"),
+            ("Burnham", "3"),
+            ("Castlefield & Oakridge", "2"),
+            ("Chalfont St Giles & Little Chalfont", "3"),
+            ("Chalfont St Peter", "2"),
+            ("Chesham North", "3"),
+            ("Chesham South", "2"),
+            ("Chiltern Villages", "1"),
+            ("Disraeli", "1"),
+            ("Downley", "1"),
+            ("Farnhams & Stoke Poges", "3"),
+            ("Flackwell Heath & The Wooburns", "3"),
+            ("Gerrards Cross & Denham", "3"),
+            ("Grendon Underwood & The Claydons", "2"),
+            ("Haddenham & Stone", "2"),
+            ("Hazlemere", "2"),
+            ("Horwood", "1"),
+            ("Iver", "2"),
+            ("Ivinghoe", "2"),
+            ("Long Crendon", "1"),
+            ("Marlow", "3"),
+            ("Marsh & Micklefield", "2"),
+            ("Newton Longville", "2"),
+            ("Penn, Tylers Green & Loudwater", "2"),
+            ("Princes Risborough", "2"),
+            ("Quainton", "1"),
+            ("Ridgeway East", "2"),
+            ("Ridgeway West", "2"),
+            ("Sands", "1"),
+            ("Terriers & Amersham Hill", "2"),
+            ("The Missendens", "3"),
+            ("Totteridge & Bowerdean", "2"),
+            ("Waddesdon", "1"),
+            ("Wendover, Halton & Stoke Mandeville", "2"),
+            ("West Wycombe & Lane End", "1"),
+            ("Winslow", "1"),
+        ]
+
+        self.assertEqual(
+            expected,
+            lgbce_review_helper.parse_eco_xml(
+                "https://www.legislation.gov.uk/uksi/2023/1205/schedule/1/made/data.xml"
+            ),
+        )
+
+    @patch("eco_parser.parser.EcoParser.get_data")
+    @patch("requests.head")
+    def test_make_eco_csv(self, requests_head_mock, get_data_mock):
+        with open(
+            "every_election/apps/organisations/boundaries/fixtures/west-suffolk-eco.xml"
+        ) as f:
+            wsk_xml = f.read()
+        get_data_mock.return_value = wsk_xml
+        requests_head_mock.return_value = mocked_head_response = Mock()
+        mocked_head_response.status_code = 200
+
+        lgbce_review_helper = LGBCEReviewHelper()
+
+        wsk_review = CompletedOrganisationBoundaryReviewFactory(
+            **{
+                "organisation": OrganisationFactory(official_identifier="WSK"),
+                "legislation_title": "The West Suffolk (Electoral Changes) Order 2018",
+                "slug": "west-suffolk",
+                "consultation_url": "http://www.lgbce.org.uk/all-reviews/west-suffolk",
+                "boundaries_url": "",
+                "status": "Completed",
+                "latest_event": "Effective date",
+                "legislation_url": "http://www.legislation.gov.uk/uksi/2018/1375/contents/made",
+                "legislation_made": True,
+            }
+        )
+
+        with open(
+            f"{os.path.dirname(os.path.realpath(__file__))}/../fixtures/west_suffolk_eco.csv"
+        ) as f:
+            expected_value = f.read()
+
+        self.assertEqual(
+            expected_value,
+            lgbce_review_helper.make_eco_csv(wsk_review).decode("utf-8"),
+        )
