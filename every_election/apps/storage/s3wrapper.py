1c4de26f685af66f66fb3cc794db86978f41ba4c
GeoWill <roper.william@gmail.com>
2024-01-08 15:25:22 +0000

    Don't look for s3 credential settings

--- every_election/apps/storage/s3wrapper.py
+++ every_election/apps/storage/s3wrapper.py
@@ -1,15 +1,12 @@
 import tempfile
 
 import boto3
-from django.conf import settings
 
 
 class S3Wrapper:
     def __init__(self, bucket_name):
         resource = boto3.resource(
             "s3",
-            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
         )
         self.bucket = resource.Bucket(bucket_name)
 
