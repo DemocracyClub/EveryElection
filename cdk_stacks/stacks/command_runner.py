f8c4e272cdf9e888f938f22b179209af2d261516
GeoWill <roper.william@gmail.com>
2024-02-16 18:33:28 +0000

    Use a geopackage instead of geojson to silence errors

--- cdk_stacks/stacks/command_runner.py
+++ cdk_stacks/stacks/command_runner.py
@@ -74,7 +74,7 @@ class EEOncePerTagCommandRunner(Stack):
             self.add_job(
                 "add_nuts1_tags",
                 "cron(15 2 * * ? *)",
-                """OGR_GEOJSON_MAX_OBJ_SIZE=500MB output-on-error ee-manage-py-command add_tags -u "https://ons-cache.s3.eu-west-1.amazonaws.com/NUTS_Level_1_(January_2018)_Boundaries.geojson" --fields '{"NUTS118NM": "value", "NUTS118CD": "key"}' --tag-name NUTS1""",
+                """output-on-error ee-manage-py-command add_tags -u "https://s3.eu-west-2.amazonaws.com/ee.public.data/ons-cache/NUTS_Level_1_(January_2018)_Boundaries.gpkg" --fields '{"NUTS118NM": "value", "NUTS118CD": "key"}' --tag-name NUTS1""",
             )
 
             # Scrape LGBCE
