0524d4f751ec3c8bb4ef51cab7b2427d78b4153b
GeoWill <roper.william@gmail.com>
2024-02-17 15:22:26 +0000

    Update serverscripts add_nuts1 script

--- serverscripts/add_nuts1.sh
+++ serverscripts/add_nuts1.sh
@@ -5,6 +5,6 @@ set -x
 cd /var/www/every_election/repo/
 source .venv/bin/activate
 python manage.py add_tags \
-    -u "https://ons-cache.s3.eu-west-1.amazonaws.com/NUTS_Level_1_(January_2018)_Boundaries.geojson" \
+    -u "https://s3.eu-west-2.amazonaws.com/ee.public.data/ons-cache/NUTS_Level_1_(January_2018)_Boundaries.gpkg" \
     --fields '{"NUTS118NM": "value", "NUTS118CD": "key"}' \
     --tag-name NUTS1
