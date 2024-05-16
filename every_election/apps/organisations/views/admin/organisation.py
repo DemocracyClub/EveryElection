97d1c6ed8031aa23f0ca53caee39215f34575ed0
Sym Roe <sym.roe@talusdesign.co.uk>
2024-05-16 08:24:31 +0100

    Show elections for an organisation in the admin

--- every_election/apps/organisations/views/admin/organisation.py
+++ every_election/apps/organisations/views/admin/organisation.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
+from elections.models import Election
+
+
+class ElectionForOrganisationInlineAdmin(admin.StackedInline):
+    model = Election
+    fields = ["election_id"]
+    can_delete = False
+    readonly_fields = ["election_id"]
+    show_change_link = True
+    extra = 0
+    ordering = ["poll_open_date"]
+
+    def has_add_permission(self, request, obj=None):
+        return False
 
 
 class OrganisationAdmin(admin.ModelAdmin):
-    search_fields = ("official_name", "common_name", "official_identifier")
-    readonly_fields = ["created", "modified"]
+    search_fields = ("common_name", "slug")
+    list_display = ("common_name", "active_period_text", "organisation_type")
+    list_filter = ("organisation_type", "territory_code", "election_types")
+    readonly_fields = [
+        "created",
+        "modified",
+        "official_identifier",
+        "organisation_type",
+        "territory_code",
+        "organisation_subtype",
+        "slug",
+    ]
+
+    inlines = [ElectionForOrganisationInlineAdmin]
