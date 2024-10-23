c85d8a339f0a17d8ade25061f9595cdac0f5aefb
chris48s <git@chris-shaw.dev>
2024-10-23 14:29:02 +0100

    install packages using uv in deploy, move gevent to prod optional group

--- deployscripts/install_python_deps.sh
+++ deployscripts/install_python_deps.sh
@@ -3,8 +3,8 @@ set -xeE
 
 # should we delete the env and recreate?
 cd /var/www/every_election/repo/
-python3 -m venv .venv
+./deployscripts/install_uv.sh
+. $HOME/.cargo/env
+uv venv
 . .venv/bin/activate
-pip install -U pip
-pip install -r requirements/production.txt
-pip install gevent
+uv sync --extra production --no-dev
