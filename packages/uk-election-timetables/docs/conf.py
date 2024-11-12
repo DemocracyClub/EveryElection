bf7dacec45f7292a6df4e1ef105d0d3ff0c50271
chris48s <git@chris-shaw.dev>
2024-11-12 13:25:41 +0000

    4.1.0

--- packages/uk-election-timetables/docs/conf.py
+++ packages/uk-election-timetables/docs/conf.py
@@ -10,9 +10,9 @@ copyright = "2020, Alex Wilson"
 author = "Alex Wilson"
 
 # The short X.Y version
-version = "4.0"
+version = "4.1"
 # The full version, including alpha/beta/rc tags
-release = "4.0.0"
+release = "4.1.0"
 
 
 # -- General configuration ---------------------------------------------------
