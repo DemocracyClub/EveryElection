747131b4eac5fc1772c318abd8e061427891aa6c
Will Roper <GeoWill@users.noreply.github.com>
2025-03-19 15:19:42 +0000

    Fix sync_map_layers_to_s3.sh path
    The old `ee-maps` bucket is in the old AWS account. 
    
    This one is in the new account, so probably makes sense to use it.

--- serverscripts/sync_map_layers_to_s3.sh
+++ serverscripts/sync_map_layers_to_s3.sh
@@ -6,5 +6,5 @@ mkdir -p $MAP_DATA_PATH
 
 ee-manage-py-command export_boundaries --output $MAP_DATA_PATH
 
-aws s3 sync $MAP_DATA_PATH s3://ee-maps/ --acl public-read --content-type "application/json"
+aws s3 sync $MAP_DATA_PATH s3://ee.public.data/ee-maps/ --acl public-read --content-type "application/json"
 rm $MAP_DATA_PATH/*
