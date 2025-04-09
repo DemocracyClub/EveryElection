5afea26dce0ca88aec4418a0c931f9be304d3f82
GeoWill <roper.william@gmail.com>
2025-04-09 15:01:21 +0100

    Tests for send_event function

--- every_election/apps/elections/baker.py
+++ every_election/apps/elections/baker.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import os
 from typing import Dict, Optional
 
 import boto3
@@ -10,12 +11,17 @@ logger = logging.getLogger(__name__)
 
 def send_event(detail: Dict, detail_type: str, source: Optional[str] = None):
     if not settings.SEND_EVENTS:
+        logger.info(f"Skipping {detail_type} because SEND_EVENTS is disabled.")
         # don't attempt to push anything in local dev/under test
         return
 
     if source is None:
         source = f"everyelection-{settings.DC_ENVIRONMENT}-{settings.EC2_IP}"
-    events_client = boto3.client("events")
+
+    session = boto3.Session(
+        region_name=os.environ.get("AWS_REGION", "eu-west-2")
+    )
+    events_client = session.client("events")
 
     try:
         entries = [
@@ -35,7 +41,7 @@ def send_event(detail: Dict, detail_type: str, source: Optional[str] = None):
                 extra={"response": response},
             )
     except (
-        # All exceptions listed in the boto3 put_events docs.
+        # Only exception listed in the boto3 put_events docs.
         events_client.exceptions.InternalException,
     ) as put_events_exception:
         # We don't want to break whatever operation we were trying to do when sending the message.
