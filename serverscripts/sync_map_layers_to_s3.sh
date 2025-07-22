4bfba7b28e72f0ceaaa8ce11bcc2c844fa9f7c1e
GeoWill <roper.william@gmail.com>
2025-07-22 16:56:21 +0100

    Use manage-py-command not ee-manage-py-command

--- serverscripts/sync_map_layers_to_s3.sh
+++ serverscripts/sync_map_layers_to_s3.sh
@@ -4,7 +4,7 @@ set -e
 MAP_DATA_PATH=/var/www/every_election/repo/every_election/data/maps
 mkdir -p $MAP_DATA_PATH
 
-ee-manage-py-command export_boundaries --output $MAP_DATA_PATH
+manage-py-command export_boundaries --output $MAP_DATA_PATH
 
 aws s3 sync $MAP_DATA_PATH s3://ee.public.data/ee-maps/ --acl public-read --content-type "application/json"
 rm $MAP_DATA_PATH/*
