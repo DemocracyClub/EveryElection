d7060a4a8253414596f40d61877671139b58f1e4
GeoWill <roper.william@gmail.com>
2025-07-22 14:33:29 +0100

    Use an initial setup script for before install
    We can delete stop_application.sh, because we're dropping the image that
    will have a running app on it.

--- deployscripts/hooks/stop_application.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -xeE
-
-service gunicorn_every_election stop
-
-# delete all code including hidden files
-rm -rf /var/www/every_election/repo/* /var/www/every_election/repo/.* 2> /dev/null || true
