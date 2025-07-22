938487663140a053b3ce72e7fccb35cdb3b60ad9
GeoWill <roper.william@gmail.com>
2025-07-22 16:43:20 +0100

    Put crontab in conf

--- deploy/hooks/afterinstall/install_crontab.sh
+++ deploy/hooks/afterinstall/install_crontab.sh
@@ -1,4 +1,7 @@
+#!/usr/bin/env bash
+set -xeE
+
 cd /var/www/every_election/repo/
-mv deploy/crontab /etc/cron.d/every_election_cron
+mv deploy/files/conf/crontab /etc/cron.d/every_election_cron
 chown root:root /etc/cron.d/every_election_cron
 chmod 755 /etc/cron.d/every_election_cron
