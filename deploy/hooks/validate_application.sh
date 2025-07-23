c80e84e64c7f930b9ecfc18da0cbce3fc3f7d699
GeoWill <roper.william@gmail.com>
2025-07-23 14:31:14 +0100

    gunicorn service naming convention

--- deploy/hooks/validate_application.sh
+++ deploy/hooks/validate_application.sh
@@ -3,4 +3,4 @@ set -xeuo pipefail
 
 # if either check returns a non-zero exit code, return exit code 1 to
 # ensure CodeDeploy recognises the validation failed
-systemctl is-active --quiet gunicorn_every_election.service || exit 1
+systemctl is-active --quiet every_election_gunicorn.service || exit 1
