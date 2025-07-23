e5b2aa4e873f024f9e1525bcf3549eb0d3352639
GeoWill <roper.william@gmail.com>
2025-07-23 14:31:14 +0100

    gunicorn service naming convention

--- deploy/hooks/start_application.sh
+++ deploy/hooks/start_application.sh
@@ -2,9 +2,9 @@
 set -xeE
 
 # Ensure gunicorn is on the right port
-sudo sed "s/127.0.0.1/0.0.0.0/g" /etc/systemd/system/gunicorn_every_election.service -i
+sudo sed "s/127.0.0.1/0.0.0.0/g" /etc/systemd/system/every_election_gunicorn.service -i
 sudo systemctl daemon-reload
 
 # enabling will allow the services to start if the instance reboots
-systemctl enable gunicorn_every_election.service
-systemctl start gunicorn_every_election.service
+systemctl enable every_election_gunicorn.service
+systemctl start every_election_gunicorn.service
