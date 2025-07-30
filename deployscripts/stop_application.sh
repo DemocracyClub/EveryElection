7785c3b78e49d082bbd72168d74120faeba3319a
GeoWill <roper.william@gmail.com>
2025-07-30 09:26:34 +0100

    Split BeforeInstall hook into two
    This let's us fail the application stop section, which is necessary when
    we move to an image which doesn't have the ee gunicorn service running
    on it.
    
    This is is because we can ignore failures in the ApplicationStop step,
    but not the BeforeInstall step.

--- deployscripts/stop_application.sh
+++ deployscripts/stop_application.sh
@@ -2,6 +2,3 @@
 set -xeE
 
 service gunicorn_every_election stop
-
-# delete all code including hidden files
-rm -rf /var/www/every_election/repo/* /var/www/every_election/repo/.* 2> /dev/null || true
