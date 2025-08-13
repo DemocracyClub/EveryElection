c91e5c232aa8dae0e30bbe47f3d257f8d998c784
awdem <wdemontmollin@gmail.com>
2025-08-13 14:55:32 +0100

    adds pmtiles command

---
+++ every_election/apps/organisations/management/commands/create_pmtile_for_divset.py
@@ -0,0 +1,94 @@
+import os
+import tempfile
+
+import boto3
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from organisations.boundaries.lgbce_review_helper import check_s3_obj_exists
+from organisations.models import OrganisationDivisionSet
+from organisations.pmtile_creator import PMtileCreator
+
+
+# TODO: implement optional overwrite arg?
+class Command(BaseCommand):
+    help = (
+        "Create a pmtile for a given divisionset using ogr2ogr and tippecanoe"
+    )
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "divisionset_id",
+            type=int,
+            help="The ID of the divisionset to generate the pmtile from",
+        )
+
+    def handle(self, *args, **options):
+        using_s3 = False
+        divset_id = options["divisionset_id"]
+        # Check divset exists
+        try:
+            divset = OrganisationDivisionSet.objects.get(id=divset_id)
+        except OrganisationDivisionSet.DoesNotExist:
+            raise CommandError(
+                f"OrganisationDivisionSet with id '{divset_id}' does not exist."
+            )
+
+        # Use S3 if PMTILES_BUCKET is set
+        if getattr(settings, "PMTILES_BUCKET", None):
+            s3_client = boto3.client("s3")
+            using_s3 = True
+        else:
+            # Make pmtiles storage directory in static
+            static_path = f"{settings.STATIC_ROOT}/pmtiles-store"
+            os.makedirs(static_path, exist_ok=True)
+
+        # Check divset has divisions
+        if divset.divisions.count() == 0:
+            raise CommandError(
+                f"OrganisationDivisionSet with id '{divset_id}' has no divisions."
+            )
+
+        # Check for existing file
+        if using_s3:
+            if check_s3_obj_exists(
+                s3_client,
+                settings.PMTILES_BUCKET,
+                divset.pmtiles_s3_key,
+            ):
+                self.stdout.write(
+                    self.style.WARNING(
+                        f"PMTile {divset.pmtiles_s3_key} already exists in S3. Skipping."
+                    )
+                )
+                return
+        else:
+            if os.path.exists(f"{static_path}/{divset.pmtiles_file_name}"):
+                self.stdout.write(
+                    self.style.WARNING(
+                        f"PMTile {divset.pmtiles_file_name} already exists. Skipping."
+                    )
+                )
+                return
+
+        pmtile_creator = PMtileCreator(divset)
+        with tempfile.TemporaryDirectory() as temp_dir:
+            pmtile_fp = pmtile_creator.create_pmtile(temp_dir)
+
+            if using_s3:
+                s3_key = divset.pmtiles_s3_key
+                s3_client.upload_file(
+                    pmtile_fp, settings.PMTILES_BUCKET, s3_key
+                )
+                self.stdout.write(
+                    self.style.SUCCESS(f"PMTile uploaded to S3 at {s3_key}.")
+                )
+            else:
+                # Move the PMTile to the static directory
+                os.rename(
+                    pmtile_fp, f"{static_path}/{divset.pmtiles_file_name}"
+                )
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        f"PMTile created at {static_path}/{divset.pmtiles_file_name}."
+                    )
+                )
---
+++ every_election/apps/organisations/tests/test_create_pmtile_for_divset.py
@@ -0,0 +1,101 @@
+import os
+import shutil
+import tempfile
+from io import StringIO
+
+import boto3
+from django.conf import settings
+from django.core.management import CommandError, call_command
+from django.test import TransactionTestCase, override_settings
+from factories import (
+    DivisionGeographyFactory,
+    OrganisationDivisionFactory,
+    OrganisationDivisionSetFactory,
+)
+from moto import mock_aws
+
+PMTILES_BUCKET = "test-pmtiles-store"
+
+
+class TestCreatePMtileForDivSet(TransactionTestCase):
+    def setUp(self):
+        self.divisionset = OrganisationDivisionSetFactory()
+        for _ in range(5):
+            div = OrganisationDivisionFactory(divisionset=self.divisionset)
+            DivisionGeographyFactory(division=div)
+        # Mock STATIC_ROOT with tmp dir
+        self.tmp_static_root = tempfile.mkdtemp()
+        self.override_static_root = override_settings(
+            STATIC_ROOT=self.tmp_static_root
+        )
+        self.override_static_root.enable()
+
+    def tearDown(self):
+        self.override_static_root.disable()
+        shutil.rmtree(self.tmp_static_root)
+
+    def test_create_pmtiles_for_divset_locally(self):
+        divset_id = self.divisionset.id
+        static_path = f"{settings.STATIC_ROOT}/pmtiles-store"
+        pmtiles_fp = f"{static_path}/{self.divisionset.pmtiles_file_name}"
+        # Ensure the PMTile file does not exist before running the command
+        self.assertFalse(os.path.exists(pmtiles_fp))
+        # Call the management command
+        call_command("create_pmtile_for_divset", divset_id)
+        # Check if the PMTile file was created
+        self.assertTrue(os.path.exists(pmtiles_fp))
+
+    def test_pmtile_already_exists_locally(self):
+        divset_id = self.divisionset.id
+        static_path = f"{settings.STATIC_ROOT}/pmtiles-store"
+        pmtiles_fp = f"{static_path}/{self.divisionset.pmtiles_file_name}"
+
+        os.makedirs(static_path, exist_ok=True)
+        with open(pmtiles_fp, "w") as f:
+            f.write("dummy data")
+
+        stdout = StringIO()
+        call_command("create_pmtile_for_divset", divset_id, stdout=stdout)
+        self.assertIn("already exists", stdout.getvalue().lower())
+
+    @mock_aws
+    @override_settings(PMTILES_BUCKET=PMTILES_BUCKET)
+    def test_create_pmtiles_for_divset_on_s3(self):
+        conn = boto3.resource("s3", region_name="us-east-1")
+        conn.create_bucket(Bucket=PMTILES_BUCKET)
+
+        divset_id = self.divisionset.id
+        # Call the management command
+        call_command("create_pmtile_for_divset", divset_id)
+        # Check if the PMTile file was created
+        pmtiles_file = conn.Object(
+            PMTILES_BUCKET, self.divisionset.pmtiles_s3_key
+        )
+
+        pmtiles_file.load()  # Should raise an error if the file does not exist
+        self.assertEqual(pmtiles_file.bucket_name, PMTILES_BUCKET)
+
+    @mock_aws
+    @override_settings(PMTILES_BUCKET=PMTILES_BUCKET)
+    def test_pmtile_already_exists_on_s3(self):
+        divset_id = self.divisionset.id
+        conn = boto3.resource("s3", region_name="us-east-1")
+        conn.create_bucket(Bucket=PMTILES_BUCKET)
+        pmtiles_file = conn.Object(
+            PMTILES_BUCKET, self.divisionset.pmtiles_s3_key
+        )
+        pmtiles_file.put(Body="dummy data")
+
+        stdout = StringIO()
+        call_command("create_pmtile_for_divset", divset_id, stdout=stdout)
+        self.assertIn("already exists", stdout.getvalue().lower())
+
+    def test_divset_not_found(self):
+        with self.assertRaises(CommandError):
+            # Use a non-existent id
+            call_command("create_pmtiles_for_divset", 1000)
+
+    def test_divset_has_no_divisions(self):
+        self.divisionset.divisions.all().delete()
+        with self.assertRaises(CommandError):
+            call_command("create_pmtiles_for_divset", self.divisionset.id)
