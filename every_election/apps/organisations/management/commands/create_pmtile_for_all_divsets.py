3514d4f05f48ff042d5c207c20eddd1467a7f8ff
awdem <wdemontmollin@gmail.com>
2025-08-14 15:42:36 +0100

    add command to create pmtiles for all divsets

---
+++ every_election/apps/organisations/management/commands/create_pmtile_for_all_divsets.py
@@ -0,0 +1,27 @@
+from django.core.management import call_command
+from django.core.management.base import BaseCommand, CommandError
+from organisations.models import OrganisationDivisionSet
+
+
+class Command(BaseCommand):
+    help = "Run create_pmtile_for_divset for every DivisionSet."
+
+    def handle(self, *args, **options):
+        failures = 0
+        for divset in OrganisationDivisionSet.objects.all():
+            self.stdout.write(f"Processing DivisionSet: {divset.id}")
+            try:
+                call_command("create_pmtiles_for_divset", divset.id)
+            except Exception as e:
+                self.stdout.write(
+                    self.style.ERROR(
+                        f"Error processing DivisionSet ID {divset.id}: {str(e)}"
+                    )
+                )
+                failures += 1
+        if failures == 0:
+            self.stdout.write(
+                self.style.SUCCESS("All DivisionSets processed successfully.")
+            )
+        else:
+            raise CommandError(f"Failed to process {failures} DivisionSets")
