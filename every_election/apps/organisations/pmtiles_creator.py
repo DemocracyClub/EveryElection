d5f220df55b644ae9dcf673ef0e42561c28329c8
awdem <wdemontmollin@gmail.com>
2025-09-01 17:35:32 +0100

    adapt PMTilesCreator to create one layer per division_type

--- every_election/apps/organisations/pmtiles_creator.py
+++ every_election/apps/organisations/pmtiles_creator.py
@@ -1,36 +1,39 @@
 import subprocess
 
+import psycopg2
 from django.db import connection
 from organisations.models import DivisionGeography
 
 
 class PMtilesCreator:
     def __init__(self, divset):
-        div_subtypes = divset.divisions.values("division_subtype").distinct()
-        if div_subtypes.count() > 1:
-            raise NotImplementedError(
-                "PMTiles creation is not implemented for multiple division subtypes."
-            )
         self.divset = divset
 
     def create_pmtile(self, dest_dir):
         pmtiles_fp = f"{dest_dir}/{self.divset.pmtiles_file_name}"
+        # Create a geojson file for each division subtype
+        div_types = set(
+            self.divset.divisions.values_list("division_type", flat=True)
+        )
+        geojson_files = []
+        for div_type in div_types:
+            geojson_fp = self.create_geojson(dest_dir, div_type)
+            geojson_files.append(geojson_fp)
 
-        geojson_fp = self.create_geojson(dest_dir)
-        tippecanoe_command = f"tippecanoe -o {pmtiles_fp} -zg --drop-rate=2 --drop-densest-as-needed {geojson_fp} -l {self.divset.id}"
+        tippecanoe_command = f"tippecanoe -o {pmtiles_fp} -zg --drop-rate=2 --drop-densest-as-needed {' '.join(geojson_files)}"
         subprocess.run(tippecanoe_command, shell=True, check=True)
 
         return pmtiles_fp
 
-    def create_geojson(self, dest_dir):
-        geojson_fp = f"{dest_dir}/{self.divset.id}.geojson"
-        ogr_command = self.construct_ogr_command(geojson_fp)
+    def create_geojson(self, dest_dir, div_type):
+        geojson_fp = f"{dest_dir}/{self.divset.id}_{div_type}.geojson"
+        ogr_command = self.construct_ogr_command(geojson_fp, div_type)
         subprocess.run(ogr_command, shell=True, check=True)
         return geojson_fp
 
-    def construct_ogr_command(self, geojson_fp):
+    def construct_ogr_command(self, geojson_fp, div_type):
         db_settings = connection.settings_dict
-        sql_query = self.construct_sql_query(self.divset.id)
+        sql_query = self.construct_sql_query(self.divset.id, div_type)
 
         db_connection_string = (
             f"dbname={db_settings['NAME']} "
@@ -46,10 +49,11 @@ class PMtilesCreator:
             f'-sql "{sql_query}"'
         )
 
-    def construct_sql_query(self, divisionset_id):
+    def construct_sql_query(self, divisionset_id, div_type):
         sql, params = (
             DivisionGeography.objects.filter(
-                division__divisionset_id=divisionset_id
+                division__divisionset_id=divisionset_id,
+                division__division_type=div_type,
             )
             .select_related("division")
             .values(
@@ -62,5 +66,14 @@ class PMtilesCreator:
             )
             .query.sql_with_params()
         )
+
         sql = sql.replace("::bytea", "")  # Remove bytea typecast
-        return sql % params  # Substitute parameters into the SQL query
+
+        # Format params for SQL
+        formatted_params = tuple(
+            psycopg2.extensions.adapt(p).getquoted().decode()
+            if isinstance(p, str)
+            else p
+            for p in params
+        )
+        return sql % formatted_params  # Substitute params into the SQL query
