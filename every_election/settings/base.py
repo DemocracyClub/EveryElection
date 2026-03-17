dc11c4dc4579e47e6301ec79930fef13501d09d1
GeoWill <roper.william@gmail.com>
2026-03-17 14:22:42 +0000

    Update SECURE_REFERRER_POLICY to strict-origin-when-cross-origin

--- every_election/settings/base.py
+++ every_election/settings/base.py
@@ -263,6 +263,16 @@ DATA_UPLOAD_MAX_NUMBER_FIELDS = None
 
 SITE_TITLE = "Every Election"
 
+# Django's SecurityMiddleware defaults to "same-origin", which prevents the browser from sending
+# the Referer header on cross-origin requests, e.g. to tile.openstreetmap.org.
+# "strict-origin-when-cross-origin" sends the origin (i.e. up to, and including the tld) as the
+# referer on cross-origin requests.
+# This doesn't give any information about which specific page on our site a user was visiting
+# (i.e. which might contain postcode/uprn) just that they came from one of our sits.
+# refs:
+#       - https://docs.djangoproject.com/en/5.2/ref/middleware/#referrer-policy
+#       - https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Referrer-Policy
+SECURE_REFERRER_POLICY = "strict-origin-when-cross-origin"
 DATA_CACHE_DIR = root("data_cache")
 
 LOGIN_REDIRECT_URL = "home"
