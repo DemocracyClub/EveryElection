bfb3d9867c18a16d15c47e1b8fa934dd94a58840
awdem <wdemontmollin@gmail.com>
2026-09-14 08:30:30 -0400

    add provisional filter to admin

--- every_election/apps/organisations/views/admin/organisation.py
+++ every_election/apps/organisations/views/admin/organisation.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 
 class OrganisationAdmin(admin.ModelAdmin):
     search_fields = ("official_name", "common_name", "official_identifier")
+    list_filter = ("provisional",)
     readonly_fields = ["created", "modified"]
